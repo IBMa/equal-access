7e8103207c8b223185ef57335284a6c6fa0e4d33
Tom Brunet <thbrunet@us.ibm.com>
2020-04-02 15:42:21 -0500

    wrong working dir

--- travis/publish/rules.sh
+++ travis/publish/rules.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Deploy rule server..."
+cd rule-server
 
 rm -rf node_modules
 rm -rf package-lock.json
@@ -10,10 +11,10 @@ echo "BlueMix API Endpoint availability"
 curl --head https://api.ng.bluemix.net/v2/info
 
 echo "Set appropriate environment variables to configure the application."
-. ./rule-server/scripts/bluemix/blueGreenDeploymentHelper.sh
+. ./scripts/bluemix/blueGreenDeploymentHelper.sh
 
 echo "Starting Blue Green Deployment Process"
-. ./rule-server/scripts/bluemix/deploy_to_bluemix.sh
+. ./scripts/bluemix/deploy_to_bluemix.sh
 RETURN_CODE=$?
 echo "The return code value from deploy_to_bluemix.sh script: ${RETURN_CODE}"
 exit ${RETURN_CODE}
