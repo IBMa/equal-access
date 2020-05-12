7768856946d3440922207e76ff955f0de548da98
Tom Brunet <thbrunet@us.ibm.com>
2020-05-12 15:39:21 -0500

    Rule messages

--- accessibility-checker-engine/src/v2/checker/accessibility/rulesets/index.ts
+++ accessibility-checker-engine/src/v2/checker/accessibility/rulesets/index.ts
@@ -22,7 +22,7 @@ let a11yRulesets: Ruleset[] = [
     id: "IBM_Accessibility",
     name: "IBM Accessibility",
     category: eRuleCategory.ACCESSIBILITY,
-    description: "Rules for WCAG 2.1 (level A and level AA) plus additional IBM checklist supplemental requirements.",
+    description: "Rules for WCAG 2.1 AA plus additional IBM checklist supplemental requirements.",
     "checkpoints": [
         {
             "num": "1.1.1",
@@ -978,7 +978,7 @@ let a11yRulesets: Ruleset[] = [
     id: "WCAG_2_1",
     name: "WCAG 2.1",
     category: eRuleCategory.ACCESSIBILITY,
-    description: "Rules for Web Content Accessibility Guidelines (WCAG ) 2.1level A and level AA. This is the current W3C recommendation and extends Web Content Accessibility Guidelines 2.0. Content that conforms to WCAG 2.1 also conforms to WCAG 2.0.", 
+    description: "Rules for WCAG 2.1 AA. This is the current W3C recommendation. Content that conforms to WCAG 2.1 also conforms to WCAG 2.0.", 
     "checkpoints": [
         {
             "num": "1.1.1",
@@ -1934,7 +1934,7 @@ let a11yRulesets: Ruleset[] = [
     id: "WCAG_2_0",
     name: "WCAG 2.0",
     category: eRuleCategory.ACCESSIBILITY,
-    description: "Rules for Web Content Accessibility Guidelines 2.0level A and level AA, published asa W3C Recommendation December 2008.",
+    description: "Rules for WCAG 2.0 AA. Referenced by US Section 508, but not the latest W3C recommendation.",
     "checkpoints": [
         {
             "num": "1.1.1",
@@ -2814,7 +2814,7 @@ let a11yRulesets: Ruleset[] = [
     id: "IBM_Accessibility_BETA",
     name: "IBM Accessibility Experimental",
     category: eRuleCategory.ACCESSIBILITY,
-    description: "Rules for WCAG 2.1 (level A and level AA) plus additional IBM checklist supplemental requirements and potential new rules in Beta.",
+    description: "Rules for WCAG 2.1 AA plus additional IBM checklist supplemental requirements and experimental rules.",
     "checkpoints": [
         {
             "num": "1.1.1",
