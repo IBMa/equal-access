f623d115e087488bd6a3107373d25935a31d29bf
zhenwan <zhennan@us.ibm.com>
2020-06-08 16:08:42 -0500

    #1217 issue #4
    Reset Selection button, Report button---Label of these controls are repeated twice

--- accessibility-checker-extension/src/ts/devtools/Header.tsx
+++ accessibility-checker-extension/src/ts/devtools/Header.tsx
@@ -37,7 +37,7 @@ interface IHeaderProps {
     startScan: () => void,
     collapseAll: () => void,
     reportHandler: () => void,
-    counts?: { 
+    counts?: {
         "total": { [key: string]: number },
         "filtered": { [key: string]: number }
     } | null
@@ -46,7 +46,7 @@ interface IHeaderProps {
 
 export default class Header extends React.Component<IHeaderProps, IHeaderState> {
     state: IHeaderState = {};
-    
+
     render() {
         let counts = this.props.counts;
 
@@ -71,26 +71,26 @@ export default class Header extends React.Component<IHeaderProps, IHeaderState> 
             || counts.total["Recommendation"] !== counts.filtered["Recommendation"];
 
         let headerContent = (<div className="bx--grid">
-            <div className="bx--row" style={{lineHeight: "1rem"}}>
+            <div className="bx--row" style={{ lineHeight: "1rem" }}>
                 <div className="bx--col-sm-3">
-                    <div className="eaacTitle"><span style={{fontWeight:"bold"}}>IBM Equal Access Accessibility Checker</span></div>
+                    <div className="eaacTitle"><span style={{ fontWeight: "bold" }}>IBM Equal Access Accessibility Checker</span></div>
                 </div>
-                <div className="bx--col-sm-1" style={{position: "relative"}}>
+                <div className="bx--col-sm-1" style={{ position: "relative" }}>
                     <img className="bee-logo" src={BeeLogo} alt="IBM Accessibility" />
                 </div>
             </div>
-            <div className="bx--row" style={{marginTop:'16px'}}>
+            <div className="bx--row" style={{ marginTop: '16px' }}>
                 <div className="bx--col-sm-2">
                     <Button onClick={this.props.startScan.bind(this)} size="small" className="scan-button">Scan</Button>
                 </div>
-                <div className="bx--col-sm-2" style={{position: "relative"}}>
+                <div className="bx--col-sm-2" style={{ position: "relative" }}>
                     <div className="headerTools" >
-                        <Button 
+                        <Button
                             disabled={!this.props.counts}
                             onClick={this.props.collapseAll}
-                            className="settingsButtons" size="small" hasIconOnly kind="ghost" iconDescription="Reset selections" type="button" 
-                            >
-                            <Reset16 aria-label="Reset selections" className="my-custom-class" />
+                            className="settingsButtons" size="small" hasIconOnly kind="ghost" iconDescription="Reset selections" type="button"
+                        >
+                            <Reset16 className="my-custom-class" />
                         </Button>
                         {/* <Button 
                             disabled={!this.props.counts}
@@ -98,44 +98,44 @@ export default class Header extends React.Component<IHeaderProps, IHeaderState> 
                             >
                             <SettingsAdjust16 aria-label="Filter" className="my-custom-class" />
                         </Button> */}
-                        <Button 
+                        <Button
                             disabled={!this.props.counts}
-                            onClick={this.props.reportHandler} 
-                            className="settingsButtons" size="small" hasIconOnly kind="ghost" iconDescription="Report" type="button" 
-                            >
-                            <ReportData16 aria-label="Report" className="my-custom-class" />
+                            onClick={this.props.reportHandler}
+                            className="settingsButtons" size="small" hasIconOnly kind="ghost" iconDescription="Report" type="button"
+                        >
+                            <ReportData16 className="my-custom-class" />
                         </Button>
                     </div>
                 </div>
             </div>
-            
+
             <div className="bx--row summary">
                 <div className="bx--col-sm-1">
                     <img src={Violation16} alt="Needs review" />
-                    <span className="summaryBarCounts">{(bDiff?counts.filtered["Violation"]+"/":"")+counts.total["Violation"]}&nbsp;<span className="summaryBarLabels">Violations</span></span>
+                    <span className="summaryBarCounts">{(bDiff ? counts.filtered["Violation"] + "/" : "") + counts.total["Violation"]}&nbsp;<span className="summaryBarLabels">Violations</span></span>
                 </div>
                 <div className="bx--col-sm-1">
                     <img src={NeedsReview16} alt="Needs review" />
-                    <span className="summaryBarCounts">{(bDiff?counts.filtered["Needs review"]+"/":"")+counts.total["Needs review"]}&nbsp;<span className="summaryBarLabels">Needs&nbsp;review</span></span>
+                    <span className="summaryBarCounts">{(bDiff ? counts.filtered["Needs review"] + "/" : "") + counts.total["Needs review"]}&nbsp;<span className="summaryBarLabels">Needs&nbsp;review</span></span>
                 </div>
                 <div className="bx--col-sm-1">
                     <img src={Recommendation16} alt="Recommendation" />
-                    <span className="summaryBarCounts">{(bDiff?counts.filtered["Recommendation"]+"/":"")+counts.total["Recommendation"]}&nbsp;<span className="summaryBarLabels">Recommendations</span></span>
+                    <span className="summaryBarCounts">{(bDiff ? counts.filtered["Recommendation"] + "/" : "") + counts.total["Recommendation"]}&nbsp;<span className="summaryBarLabels">Recommendations</span></span>
                 </div>
                 <div className="bx--col-sm-1">
-                    <span className="summaryBarCounts" style={{fontWeight:400}}>{(bDiff?counts.filtered["All"]+"/":"")+counts.total["All"]}&nbsp;Issues&nbsp;{(bDiff?"selected":"found")}</span>
+                    <span className="summaryBarCounts" style={{ fontWeight: 400 }}>{(bDiff ? counts.filtered["All"] + "/" : "") + counts.total["All"]}&nbsp;Issues&nbsp;{(bDiff ? "selected" : "found")}</span>
                 </div>
             </div>
         </div>);
 
         if (this.props.layout === "main") {
-            return <div className="fixed-header" 
-                style={{zIndex:1000, backgroundColor:"rgba(255, 255, 255, 1)", left: "50%", width: "50%"}}>
-                {headerContent}                        
+            return <div className="fixed-header"
+                style={{ zIndex: 1000, backgroundColor: "rgba(255, 255, 255, 1)", left: "50%", width: "50%" }}>
+                {headerContent}
             </div>
         } else {
-            return <div className="fixed-header" style={{zIndex:1000, backgroundColor:"rgba(255, 255, 255, 1)"}}>
-                {headerContent}            
+            return <div className="fixed-header" style={{ zIndex: 1000, backgroundColor: "rgba(255, 255, 255, 1)" }}>
+                {headerContent}
             </div>
         }
     }
