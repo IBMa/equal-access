19923abeb6a09ff1e97054a19007097925883545
Charu Pandhi <cpandhi@users.noreply.github.com>
2020-07-23 14:27:42 -0500

    Sounds good! Update accessibility-checker-extension/src/ts/usingAC/UsingACApp.tsx
    Thanks @shari
    
    Co-authored-by: Shari Trewin <63615564+sharitrewin@users.noreply.github.com>

--- accessibility-checker-extension/src/ts/usingAC/UsingACApp.tsx
+++ accessibility-checker-extension/src/ts/usingAC/UsingACApp.tsx
@@ -427,7 +427,7 @@ class UsingACApp extends React.Component<{}, UsingACAppState> {
               looking to find and fix errors quickly as they are building a
               component, while the Accessibility Assessment view provides
               explanation and suggested solutions for each issue reported.
-              <strong>Note</strong>: On rare occasions the Accessibility Checker extension does not appear in the developer tools for some sites due to a bug in the developer tools. The workaround is to go to a site that you know the checker launches, launch the checker in the developer tools and then load the site that did not launch, in the same browser tab.
+              <strong>Note</strong>: On rare occasions the Accessibility Checker extension does not appear in the developer tools for some sites due to a bug in the developer tools. The workaround is to go to a site where you know the checker will launch, and launch the checker in the developer tools. Then, in the same browser tab, load the site that did not launch.
             </p>
 
             <h3 id="a11y_check">6.1 Accessibility Checker</h3>
