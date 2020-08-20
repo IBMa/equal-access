86def4de5afe6a4c4b2dfb7a5dc9e1cd522dbf16
Tom Brunet <thbrunet@us.ibm.com>
2020-08-20 09:58:02 -0500

    New regex's for gulp files

--- report-react/gulp/gulpFile.js
+++ report-react/gulp/gulpFile.js
@@ -198,9 +198,17 @@ function copyFiles() {
         .pipe(replace(/`/g, "\\`"))
         .pipe(replace("export default ({ children, _frontmatter }) => (<React.Fragment>{children}</React.Fragment>)", ""))
         .pipe(replace("export default ({ children }) => (<React.Fragment>{children}</React.Fragment>)", ""))
+        .pipe(replace(/className=\"toolHead\">(.|\n)*?\<\/Column\>/,`className="toolHead">
+
+<ItemActive item={this.props.item} />
+
+<div id="locSnippet"></div>
+
+<ItemPassive item={this.props.item} />
+
+</Column>`))
         .pipe(replace("<div id=\"locSnippet\"></div>", "<ItemSnippet item={this.props.item} />"))
         .pipe(replace("<div id=\"locLevel\"></div>", "<ItemLevel item={this.props.item} />"))
-        .pipe(replace(/(## (.|\n)*?## (.|\n)*?## ).*?\n(.|\n)*?\n\n/, "$1<ItemActive item={this.props.item} />\n<ItemPassive item={this.props.item} />\n\n"))
         .pipe(replace(/^[^<]*/, componentHeader))
         .pipe(replace(/$/, componentFooter))
         .pipe(gulp.dest("../src/help/"));
