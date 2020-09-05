cebcb50da4875b6bc29034d78599c4113ebf1741
zhenwan <zhennan@us.ibm.com>
2020-09-05 13:55:58 -0500

    header sheet

--- accessibility-checker-extension/src/ts/multiScanReports/singlePageReport/xlsx/singlePageReport.tsx
+++ accessibility-checker-extension/src/ts/multiScanReports/singlePageReport/xlsx/singlePageReport.tsx
@@ -17,7 +17,6 @@
 import ReportUti from "../../reportUtil";
 import ReportSummaryUtil from '../../../util/reportSummaryUtil';
 
-//var Excel = require('exceljs');
 import XLSX from 'xlsx';
 const stringHash = require("string-hash");
 
@@ -67,116 +66,10 @@ export default class SinglePageReport {
         return workbook;
     }
 
-    // public static create_header_sheet(xlsx_props: any, header_sheet: any) {
-    //     console.log('----report------', xlsx_props.report);
-    //     var report = xlsx_props.report;
-    //     var tab_url = xlsx_props.tabURL;
-
-
-    //     var summaryNumbers = ReportSummaryUtil.calcSummary(report);
-    //     var element_no_failures = parseInt((((summaryNumbers[4] - summaryNumbers[3]) / summaryNumbers[4]) * 100).toFixed(0));
-    //     var element_no_violations = parseInt((((summaryNumbers[4] - summaryNumbers[0]) / summaryNumbers[4]) * 100).toFixed(0));
-
-    //     var violation = report?.counts.total.Violation;
-    //     var needs_review = report?.counts.total["Needs review"];
-    //     var recommendation = report?.counts.total.Recommendation;
-    //     //var pass = report?.counts.total.Pass;
-
-
-    //     //set column width
-    //     header_sheet.getColumn('A').width = 10;
-    //     header_sheet.getColumn('B').width = 25;
-    //     header_sheet.getColumn('C').width = 25;
-    //     header_sheet.getColumn('D').width = 40;
-    //     header_sheet.getColumn('E').width = 15;
-    //     header_sheet.getColumn('F').width = 15;
-    //     header_sheet.getColumn('G').width = 20;
-
-    //     var report_title = header_sheet.getCell('A1');
-    //     report_title.value = 'Accessibility Scan Report';
-    //     report_title.font = {
-    //         bold: true
-    //     };
-
-    //     var tool = header_sheet.getCell('A3');
-    //     var tool_info = header_sheet.getCell('B3');
-    //     tool.value = 'Tool:';
-    //     tool_info.value = 'IBM Equal Access Accessibility Checker';
-
-    //     var version = header_sheet.getCell('A4');
-    //     var version_info = header_sheet.getCell('B4');
-    //     version.value = 'Version:';
-    //     version_info.value = chrome.runtime.getManifest().version;
-
-    //     var rule_set = header_sheet.getCell('A5');
-    //     var rule_set_info = header_sheet.getCell('B5');
-    //     rule_set.value = 'Rule set:';
-    //     rule_set_info.value = report.option.deployment.name;
-
-    //     var guidelines = header_sheet.getCell('A6');
-    //     var guidelines_info = header_sheet.getCell('B6');
-    //     guidelines.value = 'Guidelines:';
-    //     guidelines_info.value = report.option.guideline.name;
-
-    //     var scan_date = header_sheet.getCell('A7');
-    //     var scan_date_info = header_sheet.getCell('B7');
-    //     scan_date.value = 'Scan date:';
-    //     scan_date_info.value = new Date(report.timestamp);
-    //     scan_date_info.alignment = { horizontal: 'left' };
-
-    //     var scans = header_sheet.getCell('A9');
-    //     var scans_info = header_sheet.getCell('B9');
-    //     scans.value = 'Scans:';
-    //     scans_info.value = '1';
-
-
-    //     var summary = header_sheet.getCell('A11');
-    //     summary.value = 'Summary';
-    //     summary.font = {
-    //         bold: true
-    //     };
-
-    //     var summary_row_header = header_sheet.getRow(13);
-    //     summary_row_header.values = [
-    //         'Page',
-    //         'Scan Label',
-    //         '% elements without violations',
-    //         '% elements without violations or items to review',
-    //         '# violations', '# needs review',
-    //         '# recommendations'];
-
-    //     var summary_row_info = header_sheet.getRow(14);
-    //     summary_row_info.values = [
-    //         tab_url,
-    //         this.format_date(report.timestamp),
-    //         element_no_violations,
-    //         element_no_failures,
-    //         violation,
-    //         needs_review,
-    //         recommendation
-    //     ];
-
-    //     return header_sheet;
-    // }
-
     public static create_header_sheet(xlsx_props: any, workbook: any) {
 
-        workbook.SheetNames.push("Header");
-
-        var ws:any = {};
-        ws["!ref"]="A1:B1";
-        ws.A1 = {v: "Accessibility Scan Report", t: "s", s: "b"}
-
-
-        workbook.Sheets["Header"] = ws;
-
-/*
-
-        console.log('----report------', xlsx_props.report);
         var report = xlsx_props.report;
         var tab_url = xlsx_props.tabURL;
-
-
         var summaryNumbers = ReportSummaryUtil.calcSummary(report);
         var element_no_failures = parseInt((((summaryNumbers[4] - summaryNumbers[3]) / summaryNumbers[4]) * 100).toFixed(0));
         var element_no_violations = parseInt((((summaryNumbers[4] - summaryNumbers[0]) / summaryNumbers[4]) * 100).toFixed(0));
@@ -184,84 +77,47 @@ export default class SinglePageReport {
         var violation = report?.counts.total.Violation;
         var needs_review = report?.counts.total["Needs review"];
         var recommendation = report?.counts.total.Recommendation;
-        //var pass = report?.counts.total.Pass;
 
+        workbook.SheetNames.push("Header");
 
-        //set column width
-        header_sheet.getColumn('A').width = 10;
-        header_sheet.getColumn('B').width = 25;
-        header_sheet.getColumn('C').width = 25;
-        header_sheet.getColumn('D').width = 40;
-        header_sheet.getColumn('E').width = 15;
-        header_sheet.getColumn('F').width = 15;
-        header_sheet.getColumn('G').width = 20;
+        var ws:any = {};
+        ws["!ref"]="A1:G14";
+        
+        ws.A1 = {v: "Accessibility Scan Report", t: "s"};
+        ws.A3 = {v: "Tool:", t: "s"}; 
+        ws.B3 = {v: "IBM Equal Access Accessibility Checker", t: "s"};
+        ws.A4 = {v: "Version:", t: "s"};
+        ws.B4 = {v: chrome.runtime.getManifest().version, t: "s"};
+        ws.A5 = {v: "Rule set:", t: "s"};
+        ws.B5 = {v: report.option.deployment.name, t: "s"};
+        ws.A6 = {v: "Guidelines:", t: "s"};
+        ws.B6 = {v: report.option.guideline.name, t: "s"};
+        ws.A7 = {v: "Scan date:", t: "s"};
+        ws.B7 = {v: new Date(report.timestamp), t: "d"};
 
-        var report_title = header_sheet.getCell('A1');
-        report_title.value = 'Accessibility Scan Report';
-        report_title.font = {
-            bold: true
-        };
+        ws.A9 = {v: "Scans:", t: "s"};
+        ws.B9 = {v: "1", t: "s"};
 
-        var tool = header_sheet.getCell('A3');
-        var tool_info = header_sheet.getCell('B3');
-        tool.value = 'Tool:';
-        tool_info.value = 'IBM Equal Access Accessibility Checker';
+        ws.A11 = {v: "Summary", t: "s"};
 
-        var version = header_sheet.getCell('A4');
-        var version_info = header_sheet.getCell('B4');
-        version.value = 'Version:';
-        version_info.value = chrome.runtime.getManifest().version;
+        ws.A13 = {v: "Page", t: "s"};
+        ws.B13 = {v: "Scan Label", t: "s"};
+        ws.C13 = {v: "% elements without violations", t: "s"};
+        ws.D13 = {v: "% elements without violations or items to review", t: "s"};
+        ws.E13 = {v: "# violations", t: "s"};
+        ws.F13 = {v: "# needs review", t: "s"};
+        ws.G13 = {v: "# recommendations", t: "s"};
 
-        var rule_set = header_sheet.getCell('A5');
-        var rule_set_info = header_sheet.getCell('B5');
-        rule_set.value = 'Rule set:';
-        rule_set_info.value = report.option.deployment.name;
+        ws.A14 = {v: tab_url, t: "s"};
+        ws.B14 = {v: this.format_date(report.timestamp), t: "s"};
+        ws.C14 = {v: element_no_violations, t: "s"};
+        ws.D14 = {v: element_no_failures, t: "s"};
+        ws.E14 = {v: violation, t: "s"};
+        ws.F14 = {v: needs_review, t: "s"};
+        ws.G14 = {v: recommendation, t: "s"};
 
-        var guidelines = header_sheet.getCell('A6');
-        var guidelines_info = header_sheet.getCell('B6');
-        guidelines.value = 'Guidelines:';
-        guidelines_info.value = report.option.guideline.name;
+        workbook.Sheets["Header"] = ws;
 
-        var scan_date = header_sheet.getCell('A7');
-        var scan_date_info = header_sheet.getCell('B7');
-        scan_date.value = 'Scan date:';
-        scan_date_info.value = new Date(report.timestamp);
-        scan_date_info.alignment = { horizontal: 'left' };
-
-        var scans = header_sheet.getCell('A9');
-        var scans_info = header_sheet.getCell('B9');
-        scans.value = 'Scans:';
-        scans_info.value = '1';
-
-
-        var summary = header_sheet.getCell('A11');
-        summary.value = 'Summary';
-        summary.font = {
-            bold: true
-        };
-
-        var summary_row_header = header_sheet.getRow(13);
-        summary_row_header.values = [
-            'Page',
-            'Scan Label',
-            '% elements without violations',
-            '% elements without violations or items to review',
-            '# violations', '# needs review',
-            '# recommendations'];
-
-        var summary_row_info = header_sheet.getRow(14);
-        summary_row_info.values = [
-            tab_url,
-            this.format_date(report.timestamp),
-            element_no_violations,
-            element_no_failures,
-            violation,
-            needs_review,
-            recommendation
-        ];
-
-        return header_sheet;
-        */
     }
     
     public static create_issues_sheet(xlsx_props: any, workbook: any) {
@@ -311,8 +167,6 @@ export default class SinglePageReport {
 
         var ws = XLSX.utils.aoa_to_sheet(ws_data);
         workbook.Sheets["Definition of fields"] = ws;
-
-        console.log('-----definition sheet ws data---', ws);
     }
 
     public static issues_sheet_rows(xlsx_props: any) {
