5abc2cc226b34603b770a119e7de662d5ef23192
drjoho <64442976+drjoho@users.noreply.github.com>
2020-09-22 10:46:54 -0500

    Adjusted size of switch button to match scan

--- accessibility-checker-extension/src/ts/devtools/Header.tsx
+++ accessibility-checker-extension/src/ts/devtools/Header.tsx
@@ -176,7 +176,7 @@ export default class Header extends React.Component<IHeaderProps, IHeaderState> 
                 <div className="bx--col-md-3">
                         <ContentSwitcher
                             title="Focused View"
-                            style={{height: "28px"}}
+                            style={{height: "30px"}}
                             selectionMode="manual"
                             selectedIndex = {1}
                             onChange={((obj:any) => {
