f4aa8af87fa891ec940ba07f334a55ede4fa7839
Tom Brunet <thbrunet@us.ibm.com>
2020-09-29 15:08:39 -0500

    Update message

--- accessibility-checker-engine/src/v2/checker/accessibility/nls/index.ts
+++ accessibility-checker-engine/src/v2/checker/accessibility/nls/index.ts
@@ -708,7 +708,7 @@ let a11yNls = {
         0: "The contrast ratio of text with its background must meet WCAG 2.1 AA requirements",
         "Pass_0": "Rule Passed",
         "Fail_1": "Text contrast of {0} with its background is less than the WCAG AA minimum requirements for text of size {1}px and weight of {2}",
-        "Potential_1": "The foreground and background color are both detected as {3}. Verify the contrast ratio of the text against the lightest and the darkest colors of the background meets the WCAG 2.1 AA minimum requirements for text of size {1}px and weight of {2}"
+        "Potential_1": "The foreground and background color are both detected as {3}. Verify the text meets the WCAG 2.1 AA requirements for text"
     },
     // JCH - DONE
     "IBMA_Color_Contrast_WCAG2AA_PV": {
