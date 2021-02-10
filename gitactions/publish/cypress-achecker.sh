f63d96dfc09d93a428ec5981277e1c95067454d0
Tom Brunet <thbrunet@us.ibm.com>
2021-02-10 13:23:12 -0600

    Debug to see if genReport is generated

--- gitactions/publish/cypress-achecker.sh
+++ gitactions/publish/cypress-achecker.sh
@@ -2,5 +2,5 @@ if [ -n "${GITHUB_REF:10}" ]; then
     cd ./cypress-accessibility-checker/package;
     echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >.npmrc;
     echo "Deploy cypress-accessibility-checker version ${GITHUB_REF:10}...";
-    ls -alh .
+    ls -alh ./src/lib/reporters
 fi
