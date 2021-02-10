77a6b56b60e068db6f07bf3f7b553a17e4044633
Tom Brunet <thbrunet@us.ibm.com>
2021-02-10 12:51:04 -0600

    publishtest

--- gitactions/publish/cypress-achecker.sh
+++ gitactions/publish/cypress-achecker.sh
@@ -1,7 +1,6 @@
-if [ -n "$RELEASE_VERSION" ]; then
+if [ -n "${GITHUB_REF:10}" ]; then
     cd ./cypress-accessibility-checker/package;
     echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >.npmrc;
-    echo "Deploy cypress-accessibility-checker version $RELEASE_VERSION...";
-    npm --no-git-tag-version version $RELEASE_VERSION;
-    npm publish;
+    echo "Deploy cypress-accessibility-checker version ${GITHUB_REF:10}...";
+    ls ./lib/reporters
 fi
