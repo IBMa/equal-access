642b2be96c85db6897bd8e237230a8473500b5ba
Tom Brunet <thbrunet@us.ibm.com>
2021-02-11 10:27:03 -0600

    Undo debug change

--- gitactions/publish/cypress-achecker.sh
+++ gitactions/publish/cypress-achecker.sh
@@ -1,6 +1,7 @@
 if [ -n "${GITHUB_REF:10}" ]; then
     cd ./cypress-accessibility-checker/package;
     echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >.npmrc;
-    echo "Deploy cypress-accessibility-checker version ${GITHUB_REF:10}...";
-    ls -alh ./src/lib/reporters
+    echo "Deploy cypress-accessibility-checker version $RELEASE_VERSION...";
+    npm --no-git-tag-version version $RELEASE_VERSION;
+    npm publish;
 fi
