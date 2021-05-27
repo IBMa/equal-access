1dfd6e182eb904e806c0ad6f67b5a0dc4fc5dc40
Tom Brunet <thbrunet@us.ibm.com>
2021-05-26 20:05:34 -0500

    Earl reporting

---
+++ accessibility-checker/test-act/ace_mapping.js
@@ -0,0 +1,173 @@
+// Aligned rules
+let ace_mapping = require("./act_aligned.json");
+// ace_mapping = {}
+// ace_mapping["bc4a75"] = [{
+    // ruleId: "Rpt_Aria_RequiredChildren_Native_Host_Sematics",
+    // reasonIds: [ "Potential_1" ]
+// }]
+/*
+// Rule defect
+ruleMapping["ff89c9"] = [{ // https://github.ibm.com/ibma/e2e/issues/3142
+    ruleId: "Rpt_Aria_RequiredParent_Native_Host_Sematics",
+    reasonIds: ["Fail_1"]
+}];
+
+// Testcase defect
+ruleMapping["6a7281"] = [ // https://github.com/act-rules/act-rules.github.io/issues/1617
+    {
+        ruleId: "Rpt_Aria_ValidPropertyValue",
+        reasonIds: ["Fail_1"]
+    },
+    {
+        ruleId: "Rpt_Aria_ValidIdRef",
+        reasonIds: ["Fail_1"]
+    }
+];
+
+ruleMapping["2ee8b8"] = [{ // https://github.com/act-rules/act-rules.github.io/issues/1618
+    ruleId: "WCAG21_Label_Accessible",
+    reasonIds: ["Fail_1"]
+}]
+
+// Missing published
+// Viewport
+ruleMapping["b4f0c3"] = []
+
+// Aligned on failures
+ruleMapping["bf051a"] = [{
+    ruleId: "WCAG20_Elem_Lang_Valid",
+    reasonIds: ["Fail_1", "Fail_3"]
+}];
+ruleMapping["5b7ae0"] = [
+    // {
+    //     ruleId: "WCAG20_Html_HasLang",
+    //     reasonIds: []
+    // }
+];
+ruleMapping["de46e4"] = [{
+    ruleId: "WCAG20_Elem_Lang_Valid",
+    reasonIds: ["Fail_1"]
+}];
+ruleMapping["674b10"] = [{
+    ruleId: "Rpt_Aria_ValidRole",
+    reasonIds: ["Fail_2"]
+}]
+
+// Not evaluated
+ruleMapping["6cfa84"] = []
+ruleMapping["a25f45"] = []
+ruleMapping["b33eff"] = []
+ruleMapping["80f0bf"] = []
+ruleMapping["46ca7f"] = []
+ruleMapping["307n5z"] = []
+ruleMapping["ffd0e9"] = []
+ruleMapping["fd3a94"] = []
+ruleMapping["b20e66"] = []
+ruleMapping["bc659a"] = []
+ruleMapping["0ssw9k"] = []
+ruleMapping["e7aa44"] = []
+ruleMapping["oj04fd"] = []
+ruleMapping["de46e4"] = [
+    // {
+    //     ruleId: "WCAG20_Elem_Lang_Valid",
+    //     reasonIds: []
+    // }
+]
+ruleMapping["4b1c6c"] = []
+ruleMapping["24afc2"] = []
+ruleMapping["78fd32"] = []
+ruleMapping["674b10"] = [
+    // {
+    //     ruleId: "Rpt_Aria_ValidRole",
+    //     reasonIds: ["Fail_1"]
+    // }
+]
+ruleMapping["d0f69e"] = []
+ruleMapping["eac66b"] = []
+ruleMapping["9e45ec"] = []
+ruleMapping["2eb176"] = [
+    // {
+    //     ruleId: "RPT_Media_AudioTrigger",
+    //     reasonIds: []
+    // }
+]
+ruleMapping["afb423"] = []
+ruleMapping["4c31df"] = []
+ruleMapping["aaa1bf"] = []
+ruleMapping["3e12e1"] = []
+ruleMapping["cf77f2"] = []
+ruleMapping["9eb3f6"] = []
+ruleMapping["b40fd1"] = []
+ruleMapping["ye5d6e"] = []
+ruleMapping["047fe0"] = []
+ruleMapping["36b590"] = []
+ruleMapping["akn7bn"] = []
+ruleMapping["5effbb"] = []
+ruleMapping["09o5cg"] = []
+ruleMapping["afw4f7"] = []
+ruleMapping["f51b46"] = [
+    // {
+    //     ruleId: "RPT_Media_VideoObjectTrigger",
+    //     reasonIds: []
+    // }
+]
+ruleMapping["ab4d13"] = []
+ruleMapping["c5a4ea"] = []
+ruleMapping["1ea59c"] = [
+    // {
+    //     ruleId: "RPT_Media_VideoReferenceTrigger",
+    //     reasonIds: []
+    // }
+]
+ruleMapping["f196ce"] = [
+    // {
+    //     ruleId: "HAAC_Video_HasNoTrack",
+    //     reasonIds: []
+    // }
+]
+ruleMapping["1ec09b"] = []
+ruleMapping["1a02b0"] = []
+ruleMapping["c3232f"] = []
+ruleMapping["d7ba54"] = []
+ruleMapping["ac7dc6"] = []
+ruleMapping["ee13b5"] = []
+ruleMapping["fd26cf"] = []
+ruleMapping["59br37"] = []
+ruleMapping["e6952f"] = []
+ruleMapping["9bd38c"] = []
+ruleMapping["7677a9"] = []
+ruleMapping["c249d5"] = []
+ruleMapping["80af7b"] = []
+ruleMapping["ebe86a"] = []
+ruleMapping["a1b64e"] = []
+ruleMapping["cc0f0a"] = [
+    // {
+    //     ruleId: "Valerie_Label_HasContent",
+    //     reasonIds: []
+    // }
+]
+ruleMapping["b49b2e"] = [
+    // {
+    //     ruleId: "RPT_Header_HasContent",
+    //     reasonIds: []
+    // }
+]
+ruleMapping["off6ek"] = []
+ruleMapping["0va7u6"] = []
+ruleMapping["ucwvc8"] = []
+ruleMapping["c4a8a4"] = [
+    // {
+    //     ruleId: "RPT_Title_Valid",
+    //     reasonIds: []
+    // }
+]
+ruleMapping["qt1vmo"] = []
+ruleMapping["e88epe"] = []
+ruleMapping["aizyf1"] = []
+ruleMapping["bisz58"] = []
+ruleMapping["ffbc54"] = []
+ruleMapping["efbfc7"] = []
+*/
+module.exports = {
+    ace_mapping
+}
---
+++ accessibility-checker/test-act/main.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+const { getTestcases, getResult } = require("./act");
+const fs = require("fs");
+(async () => {
+    let earlResult = {
+        "@context": "https://act-rules.github.io/earl-context.json",
+    	"@graph": [	]
+    }
+    let ruleTestInfo = await getTestcases();
+    let browser = await puppeteer.launch({headless: true, ignoreHTTPSErrors: true});
+    let pupPage = await browser.newPage();
+    pupPage.on('console', message =>
+        console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))    
+    await pupPage.setCacheEnabled(true);
+    await pupPage.setViewport({width:1280,height:1024});
+
+    try {
+        for (const ruleId in ruleTestInfo) {
+            console.group(`* ${ruleTestInfo[ruleId].label}`);
+            for (const testcase of ruleTestInfo[ruleId].testcases) {
+                let ext = testcase.url.substring(testcase.url.lastIndexOf("."));
+                if (ext === ".html" || ext === ".xhtml") {
+                    if (ruleTestInfo[ruleId].aceRules.length > 0) {
+                        console.group(`+ ${testcase.testcaseTitle}: ${testcase.url}`);
+                        // If no tests, don't bother loading the testcase
+                        await pupPage.goto(testcase.url, { waitUntil: 'networkidle2' });
+                    } else {
+                        console.group(`? ${testcase.testcaseTitle}: ${testcase.url}`);
+                    }
+                    let { title, result, issuesFail, issuesPass, issuesReview, issuesAll } = await getResult(pupPage, testcase.testcaseId, ruleTestInfo[ruleId].aceRules);
+                    earlResult["@graph"].push({
+                        "@type": "TestSubject",
+                        "source": `https://act-rules.github.io/testcases/${ruleId}/${testcase.testcaseId}.html`,
+                        "assertions": [
+                            {
+                                "@type": "Assertion",
+                                "test": { "title": title },
+                                "result": { "outcome": result }
+                            }
+                        ]
+                    });
+                    if (result === "earl:cantTell" && (testcase.expected === "passed" || testcase.expected === "failed")) {
+                        console.log("--Can't tell");
+                    } else if (`earl:${testcase.expected}` !== result) {
+                        if (result !== "earl:untested") {
+                            console.log(`\x1b[31m--Expected ${testcase.expected}, but returned ${result}
+Failures: ${JSON.stringify(issuesFail, null, 2)}
+Review: ${JSON.stringify(issuesReview, null, 2)}
+Pass: ${JSON.stringify(issuesPass, null, 2)}
+All: ${JSON.stringify(issuesAll.map(result => result.ruleId+":"+result.value[1]), null, 2)}\x1b[0m`);
+                            }
+                    }
+                    console.groupEnd();
+                }
+            }
+            console.groupEnd();
+        }
+    } catch (err) {
+        console.error(err);
+    }
+    fs.writeFileSync("./earlResult.json", JSON.stringify(earlResult, null, 2));
+    await browser.close();
+})();
+
