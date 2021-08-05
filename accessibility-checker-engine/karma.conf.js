f66a249b82b2f9d681c3726c27a9b1e9bb5af8c7
Shunguo Yan <shunguoy@Yan.local>
2021-08-05 09:12:06 -0500

    resolve merge conflict

--- accessibility-checker-engine/karma.conf.js
+++ accessibility-checker-engine/karma.conf.js
@@ -35,7 +35,6 @@ module.exports = (config) => {
             // { pattern: 'test/v2/checker/accessibility/rules/HAAC_Video_HasNoTrack_ruleunit/Video-HasTrackAndKindWithcaption.html', watched: true },
             // { pattern: 'test/v2/checker/accessibility/rules/HAAC_Video_HasNoTrack_ruleunit/Video-HasTrackAndKindWithcaptionDynamic.html', watched: true },
             // { pattern: 'test/v2/checker/accessibility/rules/HAAC_Video_HasNoTrack_ruleunit/Video-HasTrackAndKindWithSubtitles.html', watched: true },
-             
             { pattern: 'test/**/*_ruleunit/*.html', watched: true },
             { pattern: 'test/**/*_ruleunit/*.htm', watched: true },
             // all files ending in "_test"
