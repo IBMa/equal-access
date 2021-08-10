a7093b3d8a12e60b2dfd65732aee0bd5780755cf
Ali Unwala <aliunwala@ibm.com>
2021-08-10 14:57:14 -0500

    Added functionality to add icons to the page see issue here https://github.ibm.com/IBMa/E2E/issues/3448

--- accessibility-checker-extension/src/ts/contentScripts/draw.ts
+++ accessibility-checker-extension/src/ts/contentScripts/draw.ts
@@ -45,7 +45,14 @@ TabMessaging.addListener("DRAW_TABS_TO_CONTEXT_SCRIPTS", async (message: any) =>
             width: 100%;
             height: 100%;
             overflow: visible;
-        }`
+        }
+        .svgIconTest{
+            position: absolute;
+            pointer-events: none;
+            z-index: 1000;
+            overflow: visible;
+        }
+        `
     );
     draw();
     window.addEventListener('resize', function () {
@@ -60,11 +67,11 @@ TabMessaging.addListener("DRAW_TABS_TO_CONTEXT_SCRIPTS", async (message: any) =>
 TabMessaging.addListener("HIGHLIGHT_TABSTOP_TO_CONTEXT_SCRIPTS", async (message: any) => {
     // console.log("Message HIGHLIGHT_TABSTOP_TO_CONTEXT_SCRIPTS recieved in foreground")
     // console.log(message)
-    
+
     // Clearing any that are already highlighted
     document.querySelectorAll(".highlightSVG").forEach(e => e.classList.remove("highlightSVG"));
     // Highlighting any that are "clicked"
-    document.getElementsByClassName("circleNumber"+message.tabStopId)[0].classList.add("highlightSVG");
+    document.getElementsByClassName("circleNumber" + message.tabStopId)[0].classList.add("highlightSVG");
     return true;
 });
 
@@ -72,7 +79,7 @@ TabMessaging.addListener("HIGHLIGHT_TABSTOP_TO_CONTEXT_SCRIPTS", async (message:
 TabMessaging.addListener("DELETE_DRAW_TABS_TO_CONTEXT_SCRIPTS", async (message: any) => {
     // console.log("Message DELETE_DRAW_TABS_TO_CONTEXT_SCRIPTS recieved in foreground")
     // console.log(message)
-    
+
     deleteDrawing(".deleteMe");
     return true;
 });
@@ -124,14 +131,53 @@ function redraw() {
             makeText(centerX1, centerY1, (i + 1).toString());
         }
 
+        for (let i = 0; i < nodes.length; i++) {
+            let centerX1 = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width / 2;
+            let centerY1 = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height / 2;
+
+            makeIcons(centerX1, centerY1, null);
+        }
+
+
     }, 1)
 }
 
-function makeCircle(x1: number, y1: number, circleNumber:number) {
+function makeIcons(x1: number, y1: number, arrayOfIcons: any) {
+    let numberOfIcons = 0; // TODO ALI Needs to be dynamically set, this the the array length of the icons that need to be shown should be set to arrayOfIcons.length
+    let iconName = "" // TODO ALI Needs to be dynamically set, this is the name of the icon to draw  
+    arrayOfIcons = arrayOfIcons // TODO delete this line later. Added to remove typescript error "is declared but its value is never read."
+
+    // Bias the icons to the center of the circles
+    x1 = x1 + 15;
+    y1 = y1 - 8;
+    let iconBias = 16
+
+    for (let i = 0; i < numberOfIcons; i++) {
+        makeIcon(x1 + iconBias * i, y1, iconName);
+    }
+
+}
+
+function makeIcon(x1: number, y1: number, iconName: string) {
+    iconName = iconName; // TODO delete this line later. Added to remove typescript error "is declared but its value is never read."
+    let icon = document.getElementsByClassName('svgIcon1')[0]
+    var iconClone = icon.cloneNode(true);
+    (iconClone as HTMLElement).removeAttribute("display");
+    (iconClone as HTMLElement).classList.remove("svgIcon1");
+    (iconClone as HTMLElement).classList.add("svgIconTest");
+    (iconClone as HTMLElement).classList.add("deleteMe");
+    (iconClone as HTMLElement).style.position = "absolute";
+    (iconClone as HTMLElement).style.left = String(x1) + "px";
+    (iconClone as HTMLElement).style.top = String(y1) + "px";
+    (iconClone as HTMLElement).style.fill = "red";
+    document.getElementsByClassName('svgIcons')[0].appendChild(iconClone)
+}
+
+function makeCircle(x1: number, y1: number, circleNumber: number) {
     let circle = document.getElementsByClassName('tabCircle')[0]
     var circleClone = circle.cloneNode(true);
     (circleClone as HTMLElement).classList.add("deleteMe");
-    (circleClone as HTMLElement).classList.add("circleNumber"+circleNumber);
+    (circleClone as HTMLElement).classList.add("circleNumber" + circleNumber);
     (circleClone as HTMLElement).setAttribute('cx', String(x1));
     (circleClone as HTMLElement).setAttribute('cy', String(y1));
     (circleClone as HTMLElement).setAttribute('r', String(15));
@@ -169,6 +215,18 @@ function insertSVGIntoBody() {
         document.body.innerHTML += '<svg id="svg1"><circle id="circle" class="tabCircle" stroke="grey" stroke-width="1" fill="purple"/><text id="text" class="circleText" font-family="helvetica"  font-size="10" font-weight="normal" fill="white"/></svg>'
     }
 
+    // if (document.getElementById("svgIcon1") == null) {
+    //     document.body.innerHTML += '<svg class="svgIcon1" display="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32"> <defs> <style> .cls-1 { fill: none; } </style> </defs> <path d="M28,17v5H4V6H15V4H4A2,2,0,0,0,2,6V22a2,2,0,0,0,2,2h8v4H8v2H24V28H20V24h8a2,2,0,0,0,2-2V17ZM18,28H14V24h4Z" transform="translate(0 0)"/> <path d="M29,14H17a1,1,0,0,1-.8574-1.5144l6-10a1,1,0,0,1,1.7154,0l6,10A1,1,0,0,1,29,14ZM18.7661,12h8.4678L23,4.9436Z" transform="translate(0 0)"/> <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/> </svg>'
+    // }
+
+    if (document.getElementById("svgIcon1") == null) {
+        document.body.innerHTML += '<svg class="svgIcon1" display="none" version="1.1" id="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve"> <style type="text/css"> .st0{fill:none;} .st1{opacity:0;fill-opacity:0;} </style> <rect id="_Transparent_Rectangle_" class="st0" width="32" height="32"/> <path d="M16,2C8.3,2,2,8.3,2,16s6.3,14,14,14s14-6.3,14-14S23.7,2,16,2z M21.4,23L16,17.6L10.6,23L9,21.4l5.4-5.4L9,10.6L10.6,9 l5.4,5.4L21.4,9l1.6,1.6L17.6,16l5.4,5.4L21.4,23z"/> <path id="inner-path" class="st1" d="M21.4,23L16,17.6L10.6,23L9,21.4l5.4-5.4L9,10.6L10.6,9l5.4,5.4L21.4,9l1.6,1.6L17.6,16 l5.4,5.4L21.4,23z"/> </svg>'
+    }
+
+    if (document.getElementById("svgIcons") == null) {
+        document.body.innerHTML += '<div class="svgIcons"> </div>'
+    }
+
 }
 
 
@@ -178,31 +236,31 @@ function getNodesToDrawBettween() {
     console.log(chrome)
 
     let xpathArray = [];
-    for (let i = 0; i < tabStops.length; i++){
-        let singleXPath:any = {};
+    for (let i = 0; i < tabStops.length; i++) {
+        let singleXPath: any = {};
         singleXPath.xpath = getXPathForElement(tabStops[i])
         singleXPath.innerHTML = tabStops[i].innerHTML
         singleXPath.tagName = tabStops[i].tagName
         if (tabStops[i].children.length > 0) {
             singleXPath.name = tabStops[i].children[0].innerHTML
-        }else{
+        } else {
             singleXPath.name = tabStops[i].innerHTML
         }
         // singleXPath.innerHTML = tabStops[i].innerHTML
 
-        if(tabStops[i].tagName.toLowerCase() == "a"){
+        if (tabStops[i].tagName.toLowerCase() == "a") {
             singleXPath.role = "link"
-        }else if(tabStops[i].tagName.toLowerCase() == "button"){
+        } else if (tabStops[i].tagName.toLowerCase() == "button") {
             singleXPath.role = "button"
-        }else if(tabStops[i].tagName.toLowerCase() == "link"){
+        } else if (tabStops[i].tagName.toLowerCase() == "link") {
             singleXPath.role = "link"
-        }else if(tabStops[i].tagName.toLowerCase() == "input"){
+        } else if (tabStops[i].tagName.toLowerCase() == "input") {
             singleXPath.role = "textbox"
-        }else if(tabStops[i].tagName.toLowerCase() == "select"){
+        } else if (tabStops[i].tagName.toLowerCase() == "select") {
             singleXPath.role = "listbox"
-        }else if(tabStops[i].tagName.toLowerCase() == "textarea"){
+        } else if (tabStops[i].tagName.toLowerCase() == "textarea") {
             singleXPath.role = "textbox"
-        }else{
+        } else {
             singleXPath.role = ""
         }
 
@@ -210,16 +268,16 @@ function getNodesToDrawBettween() {
     }
 
     console.log("In the contentScripts and I am SEND_TABBING_DATA_TO_BACKGROUND")
-    ContextScriptMessaging.sendToBackground("SEND_TABBING_DATA_TO_BACKGROUND", {tabStopsData: xpathArray})
+    ContextScriptMessaging.sendToBackground("SEND_TABBING_DATA_TO_BACKGROUND", { tabStopsData: xpathArray })
 
     return tabStops;
 }
 
-function getXPathForElement(element:any) {
-    const idx:any = (sib:any, name:any) => sib 
-        ? idx(sib.previousElementSibling, name||sib.localName) + (sib.localName == name)
+function getXPathForElement(element: any) {
+    const idx: any = (sib: any, name: any) => sib
+        ? idx(sib.previousElementSibling, name || sib.localName) + (sib.localName == name)
         : 1;
-    const segs:any = (elm:any) => (!elm || elm.nodeType !== 1) 
+    const segs: any = (elm: any) => (!elm || elm.nodeType !== 1)
         ? ['']
         : [...segs(elm.parentNode), `${elm.localName.toLowerCase()}[${idx(elm)}]`];
     return segs(element).join('/');
@@ -232,5 +290,3 @@ function getXPathForElement(element:any) {
 //                         XPathResult.FIRST_ORDERED_NODE_TYPE, null) 
 //         .singleNodeValue; 
 // } 
-
-
