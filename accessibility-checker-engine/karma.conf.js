57c3db0e256a3619526ee2450eafd21516e33fee
Shunguo Yan <shunguoy@us.ibm.com>
2021-08-18 12:51:13 -0500

    modify karma conf

--- accessibility-checker-engine/karma.conf.js
+++ accessibility-checker-engine/karma.conf.js
@@ -37,8 +37,7 @@ module.exports = (config) => {
             // { pattern: 'test/v2/checker/accessibility/rules/HAAC_Video_HasNoTrack_ruleunit/Video-HasTrackAndKindWithSubtitles.html', watched: true },
             // { pattern: 'test/v2/checker/accessibility/rules/WCAG20_Input_ExplicitLabel_ruleunit/ImgRoleButtonAlt.html', watched: true },
             // { pattern: 'test/v2/checker/accessibility/rules/Hidden_ruleunit/opacity_override_test.html', watched: true },
-            //{ pattern: 'test/v2/checker/accessibility/rules/HAAC_Aria_Native_Host_Semantics_ruleunit/table_valid_scope.html', watched: true },
-
+            
             { pattern: 'test/**/*_ruleunit/*.html', watched: true },
             { pattern: 'test/**/*_ruleunit/*.htm', watched: true },
             // all files ending in "_test"
