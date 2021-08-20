3e0ad3fd5fff9fac47a838a1b5dca3a7d896f144
Ali Unwala <aliunwala@ibm.com>
2021-08-20 15:28:14 -0500

    Lots of little updates. And integration of new box/outline based design

--- accessibility-checker-extension/src/ts/contentScripts/draw.ts
+++ accessibility-checker-extension/src/ts/contentScripts/draw.ts
@@ -13,19 +13,15 @@ TabMessaging.addListener("DRAW_TABS_TO_CONTEXT_SCRIPTS", async (message: any) =>
     console.log(message)
     injectCSS(
         `#line {
-                stroke-width: 5px;
+                stroke-width: 1px;
                 stroke: black;
             }`
     );
     injectCSS(
-        `#svg1{
+        `#svgCircle{
             position: absolute;
-            pointer-events: none;
-            z-index: 1000;
             top: 0;
             left: 0;
-            width: 100%;
-            height: 100%;
             overflow: visible;
         }
         
@@ -34,22 +30,35 @@ TabMessaging.addListener("DRAW_TABS_TO_CONTEXT_SCRIPTS", async (message: any) =>
         }
         `
     );
+    // position: absolute;
+    // pointer-events: none;
+    // z-index: 1000;
+    // top: 0;
+    // left: 0;
+    // width: 100%;
+    // height: 100%;
+    // overflow: visible;
+
     injectCSS(
-        `#svg2{
+        `#svgLine{
             position: absolute;
-            pointer-events: none;
-            z-index: 1000;
             top: 0;
             left: 0;
-            width: 100%;
-            height: 100%;
             overflow: visible;
+            pointer-events: none;
         }
         .svgIconTest{
             position: absolute;
             pointer-events: none;
             z-index: 1000;
             overflow: visible;
+        }
+        .circleText{
+            pointer-events: none;
+        }
+
+        .circleSmall{
+            font-size: 7px;
         }
         `
     );
@@ -104,98 +113,204 @@ function redraw(tabStopsResults:any) {
     setTimeout(() => {
         // let nodes = getNodesToDrawBettween();
         let nodes = getNodesXpaths(tabStopsResults);
+        let offset = 3;
         nodes = convertXpathsToHtmlElements(nodes);
         console.log("nodes = ", nodes);
 
         // JCH - need for last line to return to first node
         for (let i = 0; i < nodes.length - 1; i++) {
-            let centerX1 = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width / 2
-            let centerY1 = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height / 2
-
-            let centerX2 = nodes[i + 1].getBoundingClientRect().x + nodes[i + 1].getBoundingClientRect().width / 2
-            let centerY2 = nodes[i + 1].getBoundingClientRect().y + nodes[i + 1].getBoundingClientRect().height / 2
-
-            // console.log(centerX1, centerY1, centerX2, centerY2);
-            makeLine(centerX1, centerY1, centerX2, centerY2);
-
+            makeLine(nodes[i].getBoundingClientRect().x-offset, nodes[i].getBoundingClientRect().y-offset, nodes[i+1].getBoundingClientRect().x-offset, nodes[i+1].getBoundingClientRect().y-offset);
         }
 
+        // for (let i = 0; i < nodes.length; i++) {
+        //     let centerX1 = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width / 2;
+        //     let centerY1 = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height / 2;
+
+        //     makeCircle(centerX1, centerY1, i);
+        // }
+
+        // for (let i = 0; i < nodes.length; i++) {
+        //     let centerX1 = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width / 2;
+        //     let centerY1 = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height / 2;
+
+        //     makeText(centerX1, centerY1, (i + 1).toString());
+        // }
+
+        // for (let i = 0; i < nodes.length; i++) {
+        //     let centerX1 = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width / 2;
+        //     let centerY1 = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height / 2;
+
+        //     makeIcons(centerX1, centerY1, null);
+        // }
+
         for (let i = 0; i < nodes.length; i++) {
-            let centerX1 = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width / 2;
-            let centerY1 = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height / 2;
+            let x = nodes[i].getBoundingClientRect().x - offset;
+            let xPlusWidth = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width +  offset;
 
-            makeCircle(centerX1, centerY1, i);
-        }
+            let y = nodes[i].getBoundingClientRect().y - offset;
+            let yPlusHeight = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height + offset;
 
-        for (let i = 0; i < nodes.length; i++) {
-            let centerX1 = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width / 2;
-            let centerY1 = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height / 2;
+            if(i == 32){
+                console.log("x y :",x ," ",y)
+            }
+            makeCircleSmall(x, y, i);
+            makeTextSmall(x, y, (i + 1).toString());
 
-            makeText(centerX1, centerY1, (i + 1).toString());
-        }
+            // makeLine(x, y, x+10, y+10);
 
-        for (let i = 0; i < nodes.length; i++) {
-            let centerX1 = nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width / 2;
-            let centerY1 = nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height / 2;
 
-            makeIcons(centerX1, centerY1, null);
+            makeLine(x, y, xPlusWidth, y);
+            makeLine(x, y, x, yPlusHeight);
+            makeLine(xPlusWidth, y, xPlusWidth, yPlusHeight);
+            makeLine(x, yPlusHeight, xPlusWidth, yPlusHeight);
+            // console.log("x2 y2 :",x2 ," ",y2)
+            if(i == 32){
+                console.log("x y try2:",x ," ",y)
+            }
+
+            // if(i>=1){
+                
+                
+            //     let h = nodes[i].getBoundingClientRect().height;
+            //     let w = nodes[i].getBoundingClientRect().width;
+
+            //     let y1 = (nodes[i-1].getBoundingClientRect().y + nodes[i-1].getBoundingClientRect().height)/2; 
+            //     let y2 = (nodes[i].getBoundingClientRect().y + nodes[i].getBoundingClientRect().height)/2;
+                
+            //     let x1 = (nodes[i-1].getBoundingClientRect().x + nodes[i-1].getBoundingClientRect().width)/2; 
+            //     let x2 = (nodes[i].getBoundingClientRect().x + nodes[i].getBoundingClientRect().width)/2;
+                
+            //     let s = (y2-y1)/(x2-x1);
+                
+            //     let hh = h/2;
+            //     let hw = w/2;
+
+            //     var hsw = s * w / 2;
+            //     var hsh = ( h / 2 ) / s;
+            
+    
+            //     let intersection = "";
+            //     // if( (-h/2 <= s*w/2) && (s*w/2 <= h/2) ){
+            //     if(-hh <= hsw && hsw <= hh){
+            //             if(x2 < x1){
+            //             intersection = "rightEdge"
+            //         }else if(x2 > x1){
+            //             intersection = "leftEdge"
+            //         }
+            //     }
+            //     // if((-w/2 <= s*(h/2)) && (s*(h/2) <= w/2)){
+            //     if(-hw <= hsh && hsh <= hw){
+            //             if(y2 < y1){
+            //             intersection = "topEdge"
+            //         }else if(y2 > y1){
+            //             intersection = "bottomEdge"
+            //         }
+            //     }
+            //     console.log("---------------")
+
+            //     console.log("s: ",s)
+            //     console.log("s*w/2: ",s*w/2)
+            //     console.log("s*(h/2): ", s*(h/2))
+            //     console.log("(-h/2 <= s*w/2) :",(-h/2 <= s*w/2))
+            //     console.log("(s*w/2 <= h/2) :",(s*w/2 <= h/2))
+
+            //     console.log("-w/2 <= s*(h/2) :", -w/2 <= s*(h/2))
+            //     console.log("(s*(h/2) <= w/2) :",(s*(h/2) <= w/2))
+            //     console.log("x1 y1 :",x1 ," ",y1)
+            //     console.log("x2 y2 :",x2 ," ",y2)
+            //     console.log("index ",i+1,": ",intersection)
+            // }
+
+
+
         }
 
 
     }, 1)
 }
 
-function makeIcons(x1: number, y1: number, arrayOfIcons: any) {
-    let numberOfIcons = 0; // TODO ALI Needs to be dynamically set, this the the array length of the icons that need to be shown should be set to arrayOfIcons.length
-    let iconName = "" // TODO ALI Needs to be dynamically set, this is the name of the icon to draw  
-    arrayOfIcons = arrayOfIcons // TODO delete this line later. Added to remove typescript error "is declared but its value is never read."
+// function makeIcons(x1: number, y1: number, arrayOfIcons: any) {
+//     let numberOfIcons = 0; // TODO ALI Needs to be dynamically set, this the the array length of the icons that need to be shown should be set to arrayOfIcons.length
+//     let iconName = "" // TODO ALI Needs to be dynamically set, this is the name of the icon to draw  
+//     arrayOfIcons = arrayOfIcons // TODO delete this line later. Added to remove typescript error "is declared but its value is never read."
 
-    // Bias the icons to the center of the circles
-    x1 = x1 + 15;
-    y1 = y1 - 8;
-    let iconBias = 16
+//     // Bias the icons to the center of the circles
+//     x1 = x1 + 15;
+//     y1 = y1 - 8;
+//     let iconBias = 16
 
-    for (let i = 0; i < numberOfIcons; i++) {
-        makeIcon(x1 + iconBias * i, y1, iconName);
+//     for (let i = 0; i < numberOfIcons; i++) {
+//         makeIcon(x1 + iconBias * i, y1, iconName);
+//     }
+
+// }
+
+// function makeIcon(x1: number, y1: number, iconName: string) {
+//     iconName = iconName; // TODO delete this line later. Added to remove typescript error "is declared but its value is never read."
+//     let icon = document.getElementsByClassName('svgIcon1')[0]
+//     var iconClone = icon.cloneNode(true);
+//     (iconClone as HTMLElement).removeAttribute("display");
+//     (iconClone as HTMLElement).classList.remove("svgIcon1");
+//     (iconClone as HTMLElement).classList.add("svgIconTest");
+//     (iconClone as HTMLElement).classList.add("deleteMe");
+//     (iconClone as HTMLElement).style.position = "absolute";
+//     (iconClone as HTMLElement).style.left = String(x1) + "px";
+//     (iconClone as HTMLElement).style.top = String(y1) + "px";
+//     (iconClone as HTMLElement).style.fill = "red";
+//     document.getElementsByClassName('svgIcons')[0].appendChild(iconClone)
+// }
+
+
+function makeCircleSmall(x1: number, y1: number, circleNumber: number) {
+    if(circleNumber == 32){
+        console.log("x y circle:",x1 ," ",y1)
     }
 
-}
-
-function makeIcon(x1: number, y1: number, iconName: string) {
-    iconName = iconName; // TODO delete this line later. Added to remove typescript error "is declared but its value is never read."
-    let icon = document.getElementsByClassName('svgIcon1')[0]
-    var iconClone = icon.cloneNode(true);
-    (iconClone as HTMLElement).removeAttribute("display");
-    (iconClone as HTMLElement).classList.remove("svgIcon1");
-    (iconClone as HTMLElement).classList.add("svgIconTest");
-    (iconClone as HTMLElement).classList.add("deleteMe");
-    (iconClone as HTMLElement).style.position = "absolute";
-    (iconClone as HTMLElement).style.left = String(x1) + "px";
-    (iconClone as HTMLElement).style.top = String(y1) + "px";
-    (iconClone as HTMLElement).style.fill = "red";
-    document.getElementsByClassName('svgIcons')[0].appendChild(iconClone)
-}
-
-function makeCircle(x1: number, y1: number, circleNumber: number) {
     let circle = document.getElementsByClassName('tabCircle')[0]
     var circleClone = circle.cloneNode(true);
+    // (circleClone as HTMLElement).id = "circle"+circleNumber;
     (circleClone as HTMLElement).classList.add("deleteMe");
     (circleClone as HTMLElement).classList.add("circleNumber" + circleNumber);
     (circleClone as HTMLElement).setAttribute('cx', String(x1));
     (circleClone as HTMLElement).setAttribute('cy', String(y1));
-    (circleClone as HTMLElement).setAttribute('r', String(15));
-    document.getElementById('svg1')?.appendChild(circleClone)
+    (circleClone as HTMLElement).setAttribute('r', String(7));
+    (circleClone as HTMLElement).onclick = () => {alert("You have found circle number: " + (circleNumber +1))};
+    document.getElementById('svgCircle')?.appendChild(circleClone)
 }
 
-function makeText(x1: number, y1: number, n: string) {
+function makeTextSmall(x1: number, y1: number, n: string) {
     let text = document.getElementsByClassName('circleText')[0]
     var textClone = text.cloneNode(true);
     (textClone as HTMLElement).classList.add("deleteMe");
+    (textClone as HTMLElement).classList.add("circleSmall");
     (textClone as HTMLElement).setAttribute('x', String(x1 - 3));
     (textClone as HTMLElement).setAttribute('y', String(y1 + 2));
     (textClone as HTMLElement).innerHTML = n;
-    document.getElementById('svg1')?.appendChild(textClone)
+    document.getElementById('svgCircle')?.appendChild(textClone)
 }
+
+
+// function makeCircle(x1: number, y1: number, circleNumber: number) {
+//     let circle = document.getElementsByClassName('tabCircle')[0]
+//     var circleClone = circle.cloneNode(true);
+//     (circleClone as HTMLElement).classList.add("deleteMe");
+//     (circleClone as HTMLElement).classList.add("circleNumber" + circleNumber);
+//     (circleClone as HTMLElement).setAttribute('cx', String(x1));
+//     (circleClone as HTMLElement).setAttribute('cy', String(y1));
+//     (circleClone as HTMLElement).setAttribute('r', String(10));
+//     (circleClone as HTMLElement).onclick = () => {alert("You have found circle number: " + (circleNumber +1))};
+//     document.getElementById('svgCircle')?.appendChild(circleClone)
+// }
+
+// function makeText(x1: number, y1: number, n: string) {
+//     let text = document.getElementsByClassName('circleText')[0]
+//     var textClone = text.cloneNode(true);
+//     (textClone as HTMLElement).classList.add("deleteMe");
+//     (textClone as HTMLElement).setAttribute('x', String(x1 - 3));
+//     (textClone as HTMLElement).setAttribute('y', String(y1 + 2));
+//     (textClone as HTMLElement).innerHTML = n;
+//     document.getElementById('svgCircle')?.appendChild(textClone)
+// }
 
 function makeLine(x1: number, y1: number, x2: number, y2: number) {
     let line = document.getElementsByClassName('tabLine')[0]
@@ -205,17 +320,17 @@ function makeLine(x1: number, y1: number, x2: number, y2: number) {
     (lineClone as HTMLElement).setAttribute('y1', String(y1));
     (lineClone as HTMLElement).setAttribute('x2', String(x2));
     (lineClone as HTMLElement).setAttribute('y2', String(y2));
-    document.getElementById('svg2')?.appendChild(lineClone);
+    document.getElementById('svgLine')?.appendChild(lineClone);
 }
 
 
 function insertSVGIntoBody() {
-    if (document.getElementById("svg2") == null) {
-        document.body.innerHTML += '<svg id="svg2"><line id="line" class="tabLine"/></svg>'
+    if (document.getElementById("svgLine") == null) {
+        document.body.innerHTML += '<svg id="svgLine"><line id="line" class="tabLine"/></svg>'
     }
 
-    if (document.getElementById("svg1") == null) {
-        document.body.innerHTML += '<svg id="svg1"><circle id="circle" class="tabCircle" stroke="grey" stroke-width="1" fill="purple"/><text id="text" class="circleText" font-family="helvetica"  font-size="10" font-weight="normal" fill="white"/></svg>'
+    if (document.getElementById("svgCircle") == null) {
+        document.body.innerHTML += '<svg id="svgCircle"><circle id="circle" class="tabCircle" stroke="grey" stroke-width="1" fill="purple"/><text id="text" class="circleText" font-family="helvetica"  font-size="10" font-weight="normal" fill="white"/></svg>'
     }
 
     // if (document.getElementById("svgIcon1") == null) {
