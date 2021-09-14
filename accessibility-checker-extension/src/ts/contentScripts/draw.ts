969876d6e0700d7426f32a56cf62c3f1925af0ee
Ali Unwala <aliunwala@ibm.com>
2021-09-14 17:39:50 -0500

    Removing ID duplicates from being added to cloned lines

--- accessibility-checker-extension/src/ts/contentScripts/draw.ts
+++ accessibility-checker-extension/src/ts/contentScripts/draw.ts
@@ -348,7 +348,9 @@ function makeIcon(x1: number, y1: number, iconName: string) {
     (iconClone as HTMLElement).style.position = "absolute";
     (iconClone as HTMLElement).style.left = String(x1) + "px";
     (iconClone as HTMLElement).style.top = String(y1) + "px";
-    (iconClone as HTMLElement).style.fill = "red";
+    // (iconClone as HTMLElement).style.fill = "red";
+    // (iconClone as HTMLElement).onclick = () => { alert("You have found an warning icon") };
+
     document.getElementsByClassName('svgIcons')[0].appendChild(iconClone)
 }
 
@@ -366,6 +368,7 @@ function makeCircleSmall(x1: number, y1: number, circleNumber: number) {
 
     let circle = document.getElementsByClassName('tabCircle')[0]
     var circleClone = circle.cloneNode(true);
+    (circleClone as HTMLElement).removeAttribute("id");
     // (circleClone as HTMLElement).id = "circle"+circleNumber;
     (circleClone as HTMLElement).classList.add("deleteMe");
     (circleClone as HTMLElement).classList.add("circleNumber" + circleNumber);
@@ -390,6 +393,7 @@ function makeTextSmall(x1: number, y1: number, n: string) {
 
     let text = document.getElementsByClassName('circleText')[0]
     var textClone = text.cloneNode(true);
+    (textClone as HTMLElement).removeAttribute("id");
     (textClone as HTMLElement).classList.add("deleteMe");
     (textClone as HTMLElement).classList.add("circleSmall");
 
@@ -415,6 +419,7 @@ function makeLine(x1: number, y1: number, x2: number, y2: number, CSSclass?: str
             (lineClone as HTMLElement).classList.add(CSSclass[i]);
         }
     }
+    (lineClone as HTMLElement).removeAttribute("id");
     (lineClone as HTMLElement).classList.add("deleteMe");
     (lineClone as HTMLElement).setAttribute('x1', String(x1));
     (lineClone as HTMLElement).setAttribute('y1', String(y1));
@@ -430,11 +435,11 @@ function insertSVGIntoBody() {
     }
 
     if (document.getElementById("svgCircle") == null) {
-        document.body.innerHTML += '<svg id="svgCircle"><circle id="circle" class="tabCircle" stroke="grey" stroke-width="1" fill="purple"/><text id="text" class="circleText" font-family="helvetica"  font-size="10" font-weight="normal" fill="white"/></svg>'
+        document.body.innerHTML += '<svg id="svgCircle"><circle id="circle" class="tabCircle" stroke="grey" stroke-width="1" fill="purple"/><text class="circleText" font-family="helvetica"  font-size="10" font-weight="normal" fill="white"/></svg>'
     }
 
     if (document.getElementById("svgIcon1") == null) {
-        document.body.innerHTML += '<svg class="svgIcon1" display="none" xmlns="http://www.w3.org/2000/svg" id="icon" width="12px" height="12px" viewBox="0 0 32 32"> <defs> <style> .cls-1 { fill: none; } </style> </defs> <path id="inner-path" class="cls-1" d="M16,26a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,16,26Zm-1.125-5h2.25V12h-2.25Z" style="&#10;    fill: black;&#10;"/> <path d="M16.002,6.1714h-.004L4.6487,27.9966,4.6506,28H27.3494l.0019-.0034ZM14.875,12h2.25v9h-2.25ZM16,26a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,16,26Z" style="&#10;    fill: yellow;&#10;"/> <path d="M29,30H3a1,1,0,0,1-.8872-1.4614l13-25a1,1,0,0,1,1.7744,0l13,25A1,1,0,0,1,29,30ZM4.6507,28H27.3493l.002-.0033L16.002,6.1714h-.004L4.6487,27.9967Z" style="&#10;    fill: black;&#10;"/> <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/> </svg>'
+        document.body.innerHTML += '<svg class="svgIcon1" display="none" xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 32 32"> <defs> <style> .cls-1 { fill: none; } </style> </defs> <path  class="cls-1" d="M16,26a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,16,26Zm-1.125-5h2.25V12h-2.25Z" style="&#10;    fill: black;&#10;"/> <path d="M16.002,6.1714h-.004L4.6487,27.9966,4.6506,28H27.3494l.0019-.0034ZM14.875,12h2.25v9h-2.25ZM16,26a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,16,26Z" style="&#10;    fill: yellow;&#10;"/> <path d="M29,30H3a1,1,0,0,1-.8872-1.4614l13-25a1,1,0,0,1,1.7744,0l13,25A1,1,0,0,1,29,30ZM4.6507,28H27.3493l.002-.0033L16.002,6.1714h-.004L4.6487,27.9967Z" style="&#10;    fill: black;&#10;"/> <rect data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/> </svg>'
     }
 
 
