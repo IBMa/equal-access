692de14c595dc14ea5bd6b2fb7f67f2eb1dbcd8e
drjoho <64442976+drjoho@users.noreply.github.com>
2021-10-27 15:07:47 -0500

    put karma.config.js back to original

--- accessibility-checker-engine/karma.conf.js
+++ accessibility-checker-engine/karma.conf.js
@@ -35,12 +35,10 @@ module.exports = (config) => {
             // { pattern: 'test/v2/checker/accessibility/rules/HAAC_Video_HasNoTrack_ruleunit/Video-HasTrackAndKindWithcaption.html', watched: true },
             // { pattern: 'test/v2/checker/accessibility/rules/HAAC_Video_HasNoTrack_ruleunit/Video-HasTrackAndKindWithcaptionDynamic.html', watched: true },
             // { pattern: 'test/v2/checker/accessibility/rules/HAAC_Video_HasNoTrack_ruleunit/Video-HasTrackAndKindWithSubtitles.html', watched: true },
-            // { pattern: 'test/v2/checker/accessibility/rules/table_headers_ref_valid_ruleunit/act_fail3.html', watched: true },
-            { pattern: 'test/v2/checker/accessibility/rules/HAAC_Video_HasNoTrack_ruleunit/Video-HasAriaHidden.html', watched: true },
+            //{ pattern: 'test/v2/checker/accessibility/rules/table_headers_ref_valid_ruleunit/act_fail3.html', watched: true },
 
-
-            // { pattern: 'test/**/*_ruleunit/*.html', watched: true },
-            // { pattern: 'test/**/*_ruleunit/*.htm', watched: true },
+            { pattern: 'test/**/*_ruleunit/*.html', watched: true },
+            { pattern: 'test/**/*_ruleunit/*.htm', watched: true },
             // all files ending in "_test"
             // { pattern: 'test/*_test.js', watched: true },
             { pattern: 'test/**/*_test.js', watched: true }
