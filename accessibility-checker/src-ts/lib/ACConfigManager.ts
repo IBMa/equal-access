a772ffc990193dd82bb7336cf8d2aad91dbe2a28
Tom Brunet <thbrunet@us.ibm.com>
2022-01-21 08:07:47 -0600

    Update workflows to Node 16, gatsby update (#581)
    * Update workflows to Node 16, gatsby update
    
    * Update packages
    
    * Update packages
    
    * Webpack change
    
    * Missing webpack package
    
    * Move back to webpack 4
    
    * Yaml safeLoad removed - safe by default
    
    * Node webpack does not need library name
    
    * Build issues
    
    * Remove unnecessary packages
    
    * Build errors
    
    * build error
    
    * Build errors
    
    * Build error

--- accessibility-checker/src-ts/lib/ACConfigManager.ts
+++ accessibility-checker/src-ts/lib/ACConfigManager.ts
@@ -253,7 +253,7 @@ function loadConfigFromYAMLorJSONFile() {
                 ACConstants.DEBUG && console.log("Loading as YAML file.");
 
                 // Load in as yml or yaml file and return this object
-                return YAML.safeLoad(fs.readFileSync(fileToCheck), 'utf8');
+                return YAML.load(fs.readFileSync(fileToCheck), 'utf8');
             }
         } else {
             ACConstants.DEBUG && console.log("Trying to load as json or js.");
