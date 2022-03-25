4d6764b1ded6b9347a6e9880bfee1e1d93c73fb5
Tom Brunet <thbrunet@us.ibm.com>
2022-03-25 09:27:50 -0500

    Fix dependency that's not for aria_semantics rules

--- accessibility-checker-engine/src/v4/rules/aria_semantics.ts
+++ accessibility-checker-engine/src/v4/rules/aria_semantics.ts
@@ -16,9 +16,96 @@ import { eRulePolicy, eToolkitLevel } from "../api/IRule";
 import { RPTUtil } from "../../v2/checker/accessibility/util/legacy";
 import { ARIAMapper } from "../../v2/aria/ARIAMapper";
 
+export let aria_semantics_role: Rule = {
+    id: "aria_semantics_role",
+    context: "dom:*",
+    dependencies: ["Rpt_Aria_ValidProperty"],
+    help: {
+        "en-US": {
+            "Pass_0": "aria_semantics_role.html",
+            "Fail_1": "aria_semantics_role.html",
+            "Fail_2": "aria_semantics_role.html",
+            "group": "aria_semantics_role.html"
+        }
+    },
+    messages: {
+        "en-US": {
+            "Pass_0": "Rule Passed",
+            "Fail_1": "The ARIA role '{0}' is not valid for the element <{1}>",
+            "Fail_2": "The ARIA role '{0}' is not valid for the element <{1}> and may be ignored by the browser since the element is focusable",
+            "group": "ARIA roles must be valid for the element to which they are assigned"
+        }
+    },
+    rulesets: [{
+        "id": ["IBM_Accessibility", "WCAG_2_1", "WCAG_2_0"],
+        "num": ["4.1.2"],
+        "level": eRulePolicy.VIOLATION,
+        "toolkitLevel": eToolkitLevel.LEVEL_ONE
+    }],
+    act: [],
+    run: (context: RuleContext, options?: {}, contextHierarchies?: RuleContextHierarchy): RuleResult | RuleResult[] => {
+        const ruleContext = context["dom"].node as Element;
+
+        let domRoles: string[] = [];
+        if (ruleContext.getAttribute("role") !== null) {
+            domRoles = ruleContext.getAttribute("role").trim().toLowerCase().split(/\s+/); // separated by one or more white spaces
+        }
+        let tagName = ruleContext.tagName.toLowerCase();
+        // Roles allowed on this node
+        let allowedRoles = [];
+
+        // Failing roles
+        let failRoleTokens = [];
+        // Passing roles
+        let passRoleTokens = [];
+
+        let tagProperty = RPTUtil.getElementAriaProperty(ruleContext);
+        allowedRoles = RPTUtil.getAllowedAriaRoles(ruleContext, tagProperty);
+
+
+        // Testing restrictions for each role and adding the corresponding attributes to the allowed attribute list
+        for (let i = 0; i < domRoles.length; i++) {
+            if (allowedRoles.length === 0) {
+                if (!failRoleTokens.includes(domRoles[i])) {
+                    failRoleTokens.push(domRoles[i]);
+                }
+            } else if (!allowedRoles.includes("any")) { // any role is valid so no checking here. the validity of the aria role is checked by Rpt_Aria_ValidRole
+                if (!allowedRoles.includes(domRoles[i])) {
+                    if (!failRoleTokens.includes(domRoles[i])) {
+                        failRoleTokens.push(domRoles[i]);
+                    }
+                } else if (!passRoleTokens.includes(domRoles[i])) {
+                    passRoleTokens.push(domRoles[i])
+                }
+            } else if (allowedRoles.includes("any")) {
+                if (passRoleTokens.indexOf(domRoles[i]) === -1) {
+                    passRoleTokens.push(domRoles[i]);
+                }
+            }
+        } // for loop
+        if (failRoleTokens.includes("presentation") || failRoleTokens.includes("none") && RPTUtil.isTabbable(ruleContext)) {
+            return RuleFail("Fail_2", [failRoleTokens.join(", "), tagName]);
+        } else if (failRoleTokens.length > 0) {
+            RPTUtil.setCache(ruleContext, "aria_semantics_role", "Fail_1");
+            return RuleFail("Fail_1", [failRoleTokens.join(", "), tagName]);
+        } else if (passRoleTokens.length > 0) {
+            return RulePass("Pass_0", [passRoleTokens.join(", "), tagName]);
+        } else {
+            return null;
+        }
+
+        // below for listing all allowed role and attributes.  We can delete it if we are not using it next year (2018) #283
+        //      return new ValidationResult(passed, [ruleContext], '', '', passed == true ? [] : [roleOrAttributeTokens, tagName, allowedRoleOrAttributeTokens]);
+    }
+}
+
+// This rule is in the same file because there is a dependency that aria_semantics_role runs first,
+// and the info is passed by cache, but there isn't a dependency in the Fail_2 scenario, so regular
+// dependency cannot be used
 export let aria_semantics_attribute: Rule = {
     id: "aria_semantics_attribute",
     context: "dom:*",
+    // Partially depends on aria_semantics_role
     dependencies: [],
     help: {
         "en-US": {
