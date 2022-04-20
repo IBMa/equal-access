0e44860b45ed96b10ac33d7e7f346cb7bd385aed
Tom Brunet <thbrunet@us.ibm.com>
2022-04-20 14:29:01 -0500

    Update report to Carbon 11

---
+++ report-react/src/@types/index.d.ts
@@ -0,0 +1,13 @@
+declare module "\*.svg" {
+    import React = require("react");
+    // export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+  }
+
+  declare module '@carbon/charts-react';
+
+  declare module '@carbon/react'; 
+  declare module '@carbon/icons-react/lib/Popup'; 
+  declare module '@carbon/icons-react/lib/ChevronUp'; 
+  declare module '@carbon/icons-react/lib/ChevronDown'; 
