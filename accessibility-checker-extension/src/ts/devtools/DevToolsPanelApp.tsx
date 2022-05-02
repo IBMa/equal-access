577d7022633b9251abe3d8674364b7a1ef8b0c83
drjoho <64442976+drjoho@users.noreply.github.com>
2022-05-02 13:14:34 -0500

    added more keyboard rules for the visualization

--- accessibility-checker-extension/src/ts/devtools/DevToolsPanelApp.tsx
+++ accessibility-checker-extension/src/ts/devtools/DevToolsPanelApp.tsx
@@ -483,12 +483,24 @@ export default class DevToolsPanelApp extends React.Component<IPanelProps, IPane
                 if (result.ruleId === "detector_tabbable") {
                     // there will always be at least one tab
                     tabbable?.push(result);
-                } else if (result.value[1] !== "PASS" && (result.ruleId === "Rpt_Aria_InvalidTabindexForActivedescendant" ||
-                    result.ruleId === "IBMA_Focus_Tabbable" ||
-                    result.ruleId === "Rpt_Aria_MissingKeyboardHandler" ||
+                } else if (result.value[1] !== "PASS" && 
+                    // 2.1.1 Keyboard
+                    (result.ruleId === "HAAC_Application_Role_Text" ||
+                    result.ruleId === "HAAC_Audio_Video_Triggers" ||
+                    result.ruleId === "Rpt_Aria_InvalidTabindexForActivedescendant" ||
                     result.ruleId === "Rpt_Aria_MissingFocusableChild" ||
-                    result.ruleId === "IBMA_Focus_MultiTab" ||
+                    result.ruleId === "Rpt_Aria_MissingKeyboardHandler" ||
                     result.ruleId === "RPT_Elem_EventMouseAndKey" ||
+                    // 2.4.3 Focus Order
+                    result.ruleId === "IBMA_Focus_MultiTab" ||
+                    result.ruleId === "IBMA_Focus_Tabbable" ||
+                    // 2.4.7 Focus Visible
+                    result.ruleId === "RPT_Style_HinderFocus1" ||
+                    result.ruleId === "WCAG20_Script_FocusBlurs" ||
+                        // JCH - add rule for tab stop off page
+                    // 3.2.1 On Focus
+                    result.ruleId === "WCAG20_Select_NoChangeAction" ||
+                    // 4.1.2 Name, Role, Value
                     result.ruleId === "Rpt_Aria_ValidRole")) {
                     tabbableErrors?.push(result);
                 }
