f4d95faddc1c42df1577235db44c2ae5476d9445
Shunguo Yan <shunguoy@us.ibm.com>
2022-05-03 15:38:06 -0500

    update off screen rule logic

--- accessibility-checker-engine/src/v4/rules/element_tabbable_off_screen.ts
+++ accessibility-checker-engine/src/v4/rules/element_tabbable_off_screen.ts
@@ -12,6 +12,7 @@
  *****************************************************************************/
 
 import { RPTUtil } from "../../v2/checker/accessibility/util/legacy";
+import { getDefinedStyles } from "../../v4/util/CSSUtil";
 import { Rule, RuleResult, RuleFail, RuleContext, RulePass, RuleContextHierarchy } from "../api/IRule";
 import { eRulePolicy, eToolkitLevel } from "../api/IRule";
 
@@ -41,20 +42,28 @@ export let element_tabbable_off_screen: Rule = {
     }],
     act: [],
     run: (context: RuleContext, options?: {}, contextHierarchies?: RuleContextHierarchy): RuleResult | RuleResult[] => {
-        const ruleContext = context["dom"].node as Element;
+        const ruleContext = context["dom"].node as HTMLElement;
         if (!RPTUtil.isTabbable(ruleContext))
             return null;
-
+        /**
+         * If the element has a positive top and left bounding box, Pass
+         * If the element has a :focus style that moves it, Potential. For 2, if it can be determined 
+         *     that it moves it on screen (e.g., it was -5 left, and the difference between non-focus and focus is more than 5, maybe we can pass).
+         * If the element has a negative top or negative left, and the :focus styles don’t change that, Fail.
+         */
+        
         const bounds = context["dom"].bounds;
         //in case the bounds not available
         if (!bounds) return null;
-
-        let isInScreen = bounds['top'] > 0 && bounds['left'] > 0;
-        
-        if (isInScreen) 
+        let isOnScreen = bounds['top'] > 0 && bounds['left'] > 0;
+        if (isOnScreen) 
             return RulePass("pass");
-        else 
-            return RuleFail("fail_off", [ruleContext.nodeName.toLowerCase()]);
+
+        const styles = getDefinedStyles(ruleContext, ":focus");
+        console.log("node=" + ruleContext.nodeName + ", styles=" + JSON.stringify(styles));
+
+         
+        return RuleFail("fail_off", [ruleContext.nodeName.toLowerCase()]);
 
     }
 }
