9dda9dc7387bd12e01ad55ece7fbaaeb7ee779a6
Tom Brunet <thbrunet@us.ibm.com>
2022-05-04 13:11:34 -0500

    Karma conf tweaks

--- accessibility-checker-engine/karma.conf.js
+++ accessibility-checker-engine/karma.conf.js
@@ -59,7 +59,7 @@ module.exports = (config) => {
         customLaunchers: {
             ChromeCustom: {
                 base: 'ChromeHeadless',
-                flags: ['--disable-web-security ']
+                flags: ['--disable-web-security']
             }
         },
         preprocessors: {
