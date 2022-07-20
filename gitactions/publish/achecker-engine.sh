7a576adc9781073d1ade45a32d16f6101487d854
Tom Brunet <thbrunet@us.ibm.com>
2022-07-20 14:44:01 -0500

    ACT requested filename change (#1009)
    * ACT requested filename change
    * webdriver version
    * Update boilerplate webdriver versions

--- gitactions/publish/achecker-engine.sh
+++ gitactions/publish/achecker-engine.sh
@@ -2,7 +2,7 @@ if [ -n "${GITHUB_REF:10}" ]; then
     cd ./accessibility-checker-engine/dist
     cp ../../rule-server/dist/static/archives.json ./
     cp ../../accessibility-checker/test-act/earlResult.json ./
-    cp ../../accessibility-checker/test-act-w3/earlResultW3.json ./
+    cp ../../accessibility-checker/test-act-w3/act-report-v2.json ./
     echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >.npmrc;
     NPM_VERSION="${GITHUB_REF:10}";
     echo "Deploy accessibility-checker-engine version ${NPM_VERSION}...";
