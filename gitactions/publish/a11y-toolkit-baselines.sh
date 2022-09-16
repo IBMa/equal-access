d8aa0ae80b84e055b541ebc46258be55fd67ac3a
Erick Renteria <erick.renteria@ibm.com>
2022-09-16 10:45:31 -0500

    Updating ruleArchive

--- gitactions/publish/a11y-toolkit-baselines.sh
+++ gitactions/publish/a11y-toolkit-baselines.sh
@@ -4,4 +4,4 @@ cd a11y-rule-benchmark
 echo "Installing achecker"
 npm install --save-dev accessibility-checker
 echo "Scanning list of urls from listofUls.txt with baselines"
-npx achecker --baslineFolder /baselines listofUls.txt
+npx achecker --ruleArchive preview --baslineFolder /baselines listofUls.txt
