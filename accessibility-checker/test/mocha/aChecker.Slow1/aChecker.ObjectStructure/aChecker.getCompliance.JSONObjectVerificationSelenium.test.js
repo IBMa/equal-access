5d0855c10237415996354bd3a48b08a6dd2d6a04
Shunguo Yan <shunguoy@us.ibm.com>
2022-11-04 12:16:03 -0500

    update selenium baseline #1135

--- accessibility-checker/test/mocha/aChecker.Slow1/aChecker.ObjectStructure/aChecker.getCompliance.JSONObjectVerificationSelenium.test.js
+++ accessibility-checker/test/mocha/aChecker.Slow1/aChecker.ObjectStructure/aChecker.getCompliance.JSONObjectVerificationSelenium.test.js
@@ -180,7 +180,7 @@ describe("JSON Structure Verification Selenium", function () {
                                     })
                                     //console.log("report= " + JSON.stringify(report));
                                     // Run the diff algo to get the list of differences
-                                    differences = aChecker.diffResultsWithExpected(report, expected, false);console.log("report= " + report);
+                                    differences = aChecker.diffResultsWithExpected(report, expected, false);console.log("report= " + JSON.stringify(report));
                                 }
 
                                 expect(typeof differences).to.equal("undefined", "\nDoes not follow the correct JSON structure or can't load baselines" + JSON.stringify(differences, null, '  '));
