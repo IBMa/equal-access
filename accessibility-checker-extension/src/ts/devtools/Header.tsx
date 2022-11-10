ebc98cdb64d41d997a11833cbabb0d07fac91cb0
drjoho <64442976+drjoho@users.noreply.github.com>
2022-11-10 10:54:02 -0600

    Adjust scan button id, all DONE

--- accessibility-checker-extension/src/ts/devtools/Header.tsx
+++ accessibility-checker-extension/src/ts/devtools/Header.tsx
@@ -290,7 +290,7 @@ export default class Header extends React.Component<IHeaderProps, IHeaderState> 
                 <Grid style={{ marginTop: '10px', padding: "0rem" }}>
                     <Column sm={{span: 2}} md={{span: 4}} lg={{span: 8}} style={{ display: 'flex', alignContent: 'center' }}>
 
-                        <Button disabled={this.props.scanning} renderIcon={Renew} onClick={this.props.startScan.bind(this)} size="sm" className="scan-button">Scan</Button>
+                        <Button id="scanButton" disabled={this.props.scanning} renderIcon={Renew} onClick={this.props.startScan.bind(this)} size="sm" className="scan-button">Scan</Button>
                         <Tooltip
                             align="right"
                             label="Scan options"
