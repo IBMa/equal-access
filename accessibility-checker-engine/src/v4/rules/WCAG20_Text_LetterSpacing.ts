1cf21bd97f43cf227631d8a012b2b2b538f7b028
Shunguo Yan <shunguoy@us.ibm.com>
2022-11-19 11:56:10 -0600

    update messages #1124

--- accessibility-checker-engine/src/v4/rules/WCAG20_Text_LetterSpacing.ts
+++ accessibility-checker-engine/src/v4/rules/WCAG20_Text_LetterSpacing.ts
@@ -28,8 +28,8 @@ export let WCAG20_Text_LetterSpacing: Rule = {
     messages: {
         "en-US": {
             "Pass_0": "Rule Passed",
-            "Potential_1": "Verify space characters should not be used to create space between the letters of a word",
-            "group": "CSS 'letter-spacing' should not be used to control spacing within a word"
+            "Potential_1": "Space characters should not be used to create space between the letters of a word",
+            "group": "Sapce characters should not be used to control spacing within a word"
         }
     },
     rulesets: [{
