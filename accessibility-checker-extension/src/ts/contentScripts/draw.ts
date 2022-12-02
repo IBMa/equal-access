993cebfbffc89df4cbfe2fe0afc99c9aa4d47ac0
drjoho <64442976+drjoho@users.noreply.github.com>
2022-12-02 13:51:47 -0600

    Add initial code for Notification Dot

--- accessibility-checker-extension/src/ts/contentScripts/draw.ts
+++ accessibility-checker-extension/src/ts/contentScripts/draw.ts
@@ -81,6 +81,14 @@ TabMessaging.addListener("DRAW_TABS_TO_CONTEXT_SCRIPTS", async (message: any) =>
             fill: black
         }
 
+        .svgIconTest{
+            position: absolute !important;
+            overflow: visible !important;
+            pointer-events: none !important;
+            z-index: 2147483646 !important;
+        }
+
+
         `
     );
 
@@ -662,6 +670,9 @@ function redrawErrors(tabStopsErrors: any, tabStops: any, outlines: boolean, ifr
                                 "Error"+i.toString(), nodeXpaths[i])
                     
                     makeTextSmall(x, y, "?", "textColorBlack");
+                    // JCH TODO - now we are working towards changing the Triangle to a Notification Dot
+                    // makeIcon(xPlusWidth-6, y-6, "test");  // 12px icon on top right corner
+                    // makeIcon(x, y, "test");
                 } else {
                     continue;
                 }
@@ -849,6 +860,10 @@ function redraw(tabstops: any, tabStopsErrors: any, lines: boolean, outlines: bo
                                     x-triangleLegLength/2, y+(Math.sqrt(3)/6)*triangleLegLength,
                                     x+triangleLegLength/2, y+(Math.sqrt(3)/6)*triangleLegLength,
                                     i.toString(), nodeXpaths[i]);
+
+                        // JCH TODO - now we are working towards changing the Triangle to a Notification Dot
+                        // makeIcon(xPlusWidth-6, y-6, "test");  // 12px icon on top right corner
+                        // makeIcon(x, y, "test");
                         
 
                         makeTextSmall(x, y, (i + 1).toString(), "textColorBlack");
@@ -1085,6 +1100,82 @@ function makeTriangle(x1: number, y1: number, x2: number, y2: number,x3: number,
     // console.log("Inject triangle circleNumber" + circleNumber);
     document.getElementById('svgCircle')?.appendChild(triangleClone);
 }
+
+// JCH - now we are working towards changing the Triangle to a Notification Dot
+// function makeIcon(x1: number, y1: number, iconName: string) {
+//     iconName = iconName; // TODO delete this line later. Added to remove typescript error "is declared but its value is never read."
+//     var iconClone = createSVGErrorIconTemplate();
+//     iconClone.removeAttribute("display");
+//     iconClone.classList.remove("svgIcon1");
+//     iconClone.classList.add("svgIconTest");
+//     iconClone.classList.add("deleteMe");
+//     iconClone.style.position = "absolute";
+//     iconClone.style.left = String(x1) + "px";
+//     iconClone.style.top = String(y1) + "px";
+//     // (iconClone as HTMLElement).style.fill = "red";
+//     // (iconClone as HTMLElement).onclick = () => { alert("You have found an warning icon") };
+
+//     if (document.getElementById("svgIcons") == null) {
+//         var elemDIV = document.createElement('div');
+//         elemDIV.setAttribute("class", "svgIcons");
+//         document.body.appendChild(elemDIV);
+//     }
+//     document.getElementsByClassName('svgIcons')[0].appendChild(iconClone)
+// }
+
+// function createSVGErrorIconTemplate() {
+//     // This is what we are creating:
+//     // <svg class="svgIcon1" display = "none" xmlns = "http://www.w3.org/2000/svg" width = "12px" height = "12px" viewBox = "0 0 32 32" >
+//     //     <defs>
+//     //         <style> 
+//     //            .cls-1 { fill: none; } 
+//     //         </style>
+//     //     </defs >
+//     //     <path  class="cls-1" d = "M16,26a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,16,26Zm-1.125-5h2.25V12h-2.25Z" style = "&#10;    fill: black;&#10;" />
+//     //     <path d="M16.002,6.1714h-.004L4.6487,27.9966,4.6506,28H27.3494l.0019-.0034ZM14.875,12h2.25v9h-2.25ZM16,26a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,16,26Z" style = "&#10;    fill: yellow;&#10;" />
+//     //     <path d="M29,30H3a1,1,0,0,1-.8872-1.4614l13-25a1,1,0,0,1,1.7744,0l13,25A1,1,0,0,1,29,30ZM4.6507,28H27.3493l.002-.0033L16.002,6.1714h-.004L4.6487,27.9967Z" style = "&#10;    fill: black;&#10;" />
+//     //     <rect data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width = "32" height = "32" />
+//     // </svg>
+//     var elemSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+//     elemSvg.setAttribute("class", "svgIcon1");
+//     elemSvg.setAttribute("display", "none");
+//     elemSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+//     elemSvg.setAttribute("width", "12px");
+//     elemSvg.setAttribute("height", "12px");
+//     elemSvg.setAttribute("viewBox", "0 0 32 32"); // JCH how does viewBox affect off page
+
+//     var elemDefs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+
+//     var elemStyle = document.createElement('style');
+//     elemStyle.innerText = ".cls-1 { fill: none; }"
+
+//     var elemPath1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+//     elemPath1.setAttribute("class", "cls-1");
+//     elemPath1.setAttribute("d", "M16,26a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,16,26Zm-1.125-5h2.25V12h-2.25Z");
+//     elemPath1.setAttribute("style", "&#10;    fill: black;&#10;");
+
+//     var elemPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+//     elemPath2.setAttribute("d", "M16.002,6.1714h-.004L4.6487,27.9966,4.6506,28H27.3494l.0019-.0034ZM14.875,12h2.25v9h-2.25ZM16,26a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,16,26Z");
+//     elemPath2.setAttribute("style", "&#10;    fill: yellow;&#10;");
+
+//     var elemPath3 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+//     elemPath3.setAttribute("d", "M29,30H3a1,1,0,0,1-.8872-1.4614l13-25a1,1,0,0,1,1.7744,0l13,25A1,1,0,0,1,29,30ZM4.6507,28H27.3493l.002-.0033L16.002,6.1714h-.004L4.6487,27.9967Z");
+//     elemPath3.setAttribute("style", "&#10;    fill: black;&#10;");
+
+//     var elemRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+//     elemRect.setAttribute("data-name", "&lt;Transparent Rectangle&gt;");
+//     elemRect.setAttribute("class", "cls-1");
+//     elemRect.setAttribute("width", "32");
+//     elemRect.setAttribute("height", "32");
+
+//     elemDefs.appendChild(elemStyle);
+//     elemSvg.appendChild(elemDefs);
+//     elemSvg.appendChild(elemPath1);
+//     elemSvg.appendChild(elemPath2);
+//     elemSvg.appendChild(elemPath3);
+//     elemSvg.appendChild(elemRect);
+//     return elemSvg;
+// }
 
 
 
