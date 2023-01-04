36f419831f086cc60dbb96acd75750e0a5c879c9
Tom Brunet <thbrunet@us.ibm.com>
2023-01-04 13:32:32 -0600

    Run CE and CF

--- gitactions/publish/rules.sh
+++ gitactions/publish/rules.sh
@@ -8,33 +8,46 @@ ibmcloud cr login
 ibmcloud target -g Accessibility_Enablement_prod
 ibmcloud ce project select -n able
 
-if [[ ( "${GITHUB_REF}" =~ "refs/heads/") ]]; then
-    if [[ ( "${GITHUB_REF:11}" == "master" )]]; then
-        echo "Deploying :main"
-        docker build --tag rules:main .
-        docker tag rules:main us.icr.io/able/rules:main
-        docker push us.icr.io/able/rules:main
-        ibmcloud ce app update --name rules-main
-    else
-        echo "Deploying :sandbox"
-        docker build --tag rules:sandbox .
-        docker tag rules:sandbox us.icr.io/able/rules:sandbox
-        docker push us.icr.io/able/rules:sandbox
-        ibmcloud ce app update --name rules-sandbox
-    fi
-elif [[ ( "${GITHUB_REF}" =~ "refs/pull/") ]]; then
+if [[ "$TRAVIS_BRANCH" == "master" ]]
+    echo "Deploying :main"
+    docker build --tag rules:main .
+    docker tag rules:main us.icr.io/able/rules:main
+    docker push us.icr.io/able/rules:main
+    ibmcloud ce app update --name rules-main
+elif [[ "$TRAVIS_BRANCH" == "sandbox" ]]
     echo "Deploying :sandbox"
     docker build --tag rules:sandbox .
     docker tag rules:sandbox us.icr.io/able/rules:sandbox
     docker push us.icr.io/able/rules:sandbox
     ibmcloud ce app update --name rules-sandbox
-elif [[ ( "${GITHUB_REF}" =~ "refs/tags/") ]]; then
+elif [[ "$TRAVIS_BRANCH" == "prod" ]]
     echo "Deploying :prod"
     docker build --tag rules:prod .
     docker tag rules:prod us.icr.io/able/rules:prod
     docker push us.icr.io/able/rules:prod
     ibmcloud ce app update --name rules-prod
 fi
+
 # Note: This moves untagged image to the trash, where they will sit for 30 days
 ibmcloud cr image-prune-untagged --force --restrict able
 ibmcloud cr image-list
+
+echo "Deploy CF rule server..."
+cd rule-server
+
+rm -rf node_modules
+rm -rf package-lock.json
+
+echo "Ensure that the BlueMix API endpoint is accessible"
+
+echo "BlueMix API Endpoint availability"
+curl --head https://api.ng.bluemix.net/v2/info
+
+echo "Set appropriate environment variables to configure the application."
+. ./scripts/bluemix/blueGreenDeploymentHelper.sh
+
+echo "Starting Blue Green Deployment Process"
+. ./scripts/bluemix/deploy_to_bluemix.sh
+RETURN_CODE=$?
+echo "The return code value from deploy_to_bluemix.sh script: ${RETURN_CODE}"
+exit ${RETURN_CODE}
