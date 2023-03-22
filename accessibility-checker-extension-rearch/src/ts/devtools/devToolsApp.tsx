10ee2260d9de988b2a82d010c1183e63559fd66b
Tom Brunet <thbrunet@us.ibm.com>
2023-03-22 14:52:13 -0500

    Background color

--- accessibility-checker-extension-rearch/src/ts/devtools/devToolsApp.tsx
+++ accessibility-checker-extension-rearch/src/ts/devtools/devToolsApp.tsx
@@ -91,7 +91,7 @@ export class DevToolsApp extends React.Component<DevToolsAppProps, DevToolsAppSt
         </>;
 
         return <>
-            <Grid fullWidth={true} narrow={true} style={{padding: "0rem", minHeight: "100%", maxHeight: "100%", height: "100%"}}>
+            <Grid fullWidth={true} narrow={true} className="primaryColumn" style={{padding: "0rem", minHeight: "100%", maxHeight: "100%", height: "100%"}}>
                 <Column sm={4} md={8} lg={8} style={{margin: "0rem", minHeight: "100%", maxHeight: "100%", height: "100%" }}>
                     <div style={{ width: "calc(100% - 1rem", minHeight: "100%", maxHeight: "100%", height: "100%" }}>
                         {primaryPanel}
