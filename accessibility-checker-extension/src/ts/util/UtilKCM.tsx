0820e4ad5f928e6b9d19ea49f877af886deaf376
Josiah Hoskins <Josiah.Hoskins@ibm.com>
2023-05-01 10:36:39 -0500

    Update Keyboard Rules

--- accessibility-checker-extension/src/ts/util/UtilKCM.tsx
+++ accessibility-checker-extension/src/ts/util/UtilKCM.tsx
@@ -12,25 +12,51 @@ export class UtilKCM {
                 // 14 Keyboard Mode Rules
                 // 2.1.1 Keyboard
                 (result.ruleId === "HAAC_Application_Role_Text" ||
-                result.ruleId === "HAAC_Audio_Video_Triggers" ||
+                result.ruleId === "application_content_accessible" ||
+
+                result.ruleId === "HAAC_Audio_Video_Trigger" ||
+                result.ruleId === "media_keyboard_controllable" ||
+
                 result.ruleId === "Rpt_Aria_InvalidTabindexForActivedescendant" ||
+                result.ruleId === "aria_activedescendant_tabindex_valid" ||
+
                 result.ruleId === "Rpt_Aria_MissingFocusableChild" ||
+                result.ruleId === "aria_child_tabbable" ||
+
                 result.ruleId === "Rpt_Aria_MissingKeyboardHandler" ||
+                result.ruleId === "aria_keyboard_handler_exists" ||
+
                 result.ruleId === "RPT_Elem_EventMouseAndKey" ||
+                result.ruleId === "element_mouseevent_keyboard" ||
+
                 result.ruleId === "iframe_interactive_tabbable" ||
                 result.ruleId === "element_scrollable_tabbable" ||
+
                 // 2.4.3 Focus Order
                 result.ruleId === "IBMA_Focus_MultiTab" ||
+                result.ruleId === "widget_tabbable_single" ||
+
                 result.ruleId === "IBMA_Focus_Tabbable" ||
+
                 result.ruleId === "element_tabbable_role_valid" ||
+
                 // 2.4.7 Focus Visible
                 // result.ruleId === "RPT_Style_HinderFocus1" ||
+                // result.ruleId === "style_focus_visible" ||
+
                 result.ruleId === "WCAG20_Script_FocusBlurs" ||
+                result.ruleId === "script_focus_blur_review" ||
+
                 result.ruleId === "element_tabbable_visible" ||
+                
+
                 // 3.2.1 On Focus
                 result.ruleId === "WCAG20_Select_NoChangeAction" ||
+                result.ruleId === "script_select_review" ||
+
                 // 4.1.2 Name, Role, Value
-                result.ruleId === "Rpt_Aria_ValidRole")) {
+                result.ruleId === "Rpt_Aria_ValidRole" ||
+                result.ruleId === "aria_role_valid")) {
                 tabbableErrors?.push(result);
             }
         });
