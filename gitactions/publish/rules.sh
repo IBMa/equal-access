99833d6e56fbf0b11054dba55f681473a1d3ce8a
Tom Brunet <thbrunet@us.ibm.com>
2023-05-23 14:47:28 -0500

    Remove artifacts from discontinued Cloud Foundry environment (#1465)

--- gitactions/publish/rules.sh
+++ gitactions/publish/rules.sh
@@ -15,14 +15,6 @@ then
     docker push us.icr.io/able/rules:main
     ibmcloud ce project select -n able-main
     ibmcloud ce app update --name rules-main
-elif [[ "$TRAVIS_BRANCH" == "sandbox" ]]
-then
-    echo "Deploying :sandbox"
-    docker build --tag rules:sandbox .
-    docker tag rules:sandbox us.icr.io/able/rules:sandbox
-    docker push us.icr.io/able/rules:sandbox
-    ibmcloud ce project select -n able-sandbox
-    ibmcloud ce app update --name rules-sandbox
 elif [[ "$TRAVIS_BRANCH" == "prod" ]]
 then
     echo "Deploying :prod"
@@ -31,31 +23,15 @@ then
     docker push us.icr.io/able/rules:prod
     ibmcloud ce project select -n able
     ibmcloud ce app update --name rules-prod
+else
+    echo "Deploying :sandbox"
+    docker build --tag rules:sandbox .
+    docker tag rules:sandbox us.icr.io/able/rules:sandbox
+    docker push us.icr.io/able/rules:sandbox
+    ibmcloud ce project select -n able-sandbox
+    ibmcloud ce app update --name rules-sandbox
 fi
 
 # Note: This moves untagged image to the trash, where they will sit for 30 days
 ibmcloud cr image-prune-untagged --force --restrict able
 ibmcloud cr image-list
-
-if [[ "$TRAVIS_BRANCH" != "sandbox" ]]
-then
-    echo "Deploy CF rule server..."
-    cd rule-server
-
-    rm -rf node_modules
-    rm -rf package-lock.json
-
-    echo "Ensure that the BlueMix API endpoint is accessible"
-
-    echo "BlueMix API Endpoint availability"
-    curl --head https://api.ng.bluemix.net/v2/info
-
-    echo "Set appropriate environment variables to configure the application."
-    . ./scripts/bluemix/blueGreenDeploymentHelper.sh
-
-    echo "Starting Blue Green Deployment Process"
-    . ./scripts/bluemix/deploy_to_bluemix.sh
-    RETURN_CODE=$?
-    echo "The return code value from deploy_to_bluemix.sh script: ${RETURN_CODE}"
-    exit ${RETURN_CODE}
-fi
