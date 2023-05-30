f281d223eed5f06091dfa7757d73b7755e4d8713
Shunguo Yan <shunguoy@us.ibm.com>
2023-05-30 15:22:14 -0500

    add test cases #1438

--- accessibility-checker-engine/src/v4/rules/aria_accessiblename_exists.ts
+++ accessibility-checker-engine/src/v4/rules/aria_accessiblename_exists.ts
@@ -19,9 +19,8 @@ import { ARIADefinitions } from "../../v2/aria/ARIADefinitions";
 
 export let aria_accessiblename_exists: Rule = {
     id: "aria_accessiblename_exists",
-    context: "aria:alertdialog, aria:application, aria:button, aria:checkbox, aria:columnheader,   aria:combobox, aria:form, aria:heading, aria:img, aria:link, aria:meter, aria:menuitem, aria:menuitemcheckbox, aria:menuitemradio, aria:option, aria:progressbar, aria:radio, aria:radiogroup, aria:region,  aria:rowheader, aria:searchbox, aria:slider, aria:spinbutton, aria:switch,  aria:tab, aria:table, aria:tabpanel, aria:textbox, aria:treegrid, aria:treeitem",
-    //context: "aria:button,aria:checkbox,aria:combobox,aria:listbox,aria:menuitemcheckbox,aria:menuitemradio,aria:radio,aria:searchbox,aria:slider,aria:spinbutton,aria:switch,aria:textbox,aria:progressbar,dom:input[type=file],dom:output,dom:meter,dom:input[type=password]",
-    dependencies: ["aria_role_valid", "aria_role_redundant"],
+    context: "aria:alertdialog, aria:application, aria:columnheader, aria:form, aria:heading, aria:img, aria:link, aria:menuitem, aria:option, aria:radiogroup, aria:region,  aria:rowheader, aria:tab, aria:table, aria:tabpanel, aria:treegrid, aria:treeitem",
+    dependencies: ["aria_role_redundant"],
     help: {
         "en-US": {
             "pass": "aria_accessiblename_exists.html",
@@ -48,18 +47,13 @@ export let aria_accessiblename_exists: Rule = {
         //skip the rule
         if (VisUtil.isNodeHiddenFromAT(ruleContext)) return null;
 
-        let nodeName = ruleContext.nodeName.toLowerCase();
-        //ignore datalist element check since it will be part of a input element or hidden by default
-        if (nodeName === 'datalist')
-            return null;
-        
-        if (RPTUtil.attributeNonEmpty(ruleContext, "aria-label") || RPTUtil.attributeNonEmpty(ruleContext, "aria-labelledby") || RPTUtil.attributeNonEmpty(ruleContext, "title")) {
+        if (!RPTUtil.attributeNonEmpty(ruleContext, "aria-label") && !RPTUtil.attributeNonEmpty(ruleContext, "aria-labelledby") && !RPTUtil.attributeNonEmpty(ruleContext, "title")) {
             let roles = RPTUtil.getRoles(ruleContext, true);
             if (roles && roles.length > 0) {
                 //when multiple roles specified, only the first valid role is applied, and the others just as fallbacks
                 if (ARIADefinitions.designPatterns[roles[0]].nameFrom && ARIADefinitions.designPatterns[roles[0]].nameFrom.includes("contents")) {
                     if (!RPTUtil.getInnerText(ruleContext) || RPTUtil.getInnerText(ruleContext).trim().length === 0)
-                        return RuleFail("fail_no_accessible_name", [nodeName, roles[0]]);  
+                        return RuleFail("fail_no_accessible_name", [ruleContext.nodeName.toLowerCase(), roles[0]]);  
                 }
             } 
         }
