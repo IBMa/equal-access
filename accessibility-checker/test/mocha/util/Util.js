907ff5a8f6c036b551612f583504fe0e2f3c940c
Tom Brunet <thbrunet@us.ibm.com>
2023-06-22 20:52:05 -0500

    Use regular get/goto. Script replacement not accurate

--- accessibility-checker/test/mocha/util/Util.js
+++ accessibility-checker/test/mocha/util/Util.js
@@ -2,16 +2,18 @@ const fs = require("fs");
 
 module.exports = {
     loadSeleniumTestFile: (browser, testFile) => {
-        // return browser.get("file://" + unitTestFile);
-        let testContent = fs.readFileSync(testFile);
-        return browser.executeScript(`document.open("text/html");document.write(${JSON.stringify(testContent)});document.close()`);
+        return browser.get("file://" + testFile);
+        // let testContent = fs.readFileSync(testFile);
+        // return browser.executeScript(`document.open("text/html");document.write(${JSON.stringify(testContent)});document.close()`);
     },
     loadPuppeteerTestFile: (page, testFile) => {
-        let testContent = fs.readFileSync(testFile);
-        return page.evaluate((testContent) => {
-            document.open("text/html");
-            document.write(testContent);
-            document.close()
-        }, testContent);
+        return page.goto("file://" + testFile);
+
+        // let testContent = fs.readFileSync(testFile);
+        // return page.evaluate((testContent) => {
+        //     document.open("text/html");
+        //     document.write(testContent);
+        //     document.close()
+        // }, testContent);
     }
 }
