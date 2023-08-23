3ad5e8b8a537951cf379151ede6398a9c5aacba2
Tom Brunet <thbrunet@us.ibm.com>
2023-08-23 14:11:15 -0500

    Update other jest boilerplates

---
+++ accessibility-checker/boilerplates/jest-selenium/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "moduleNameMapper": {
+        // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+        // Note that this comes through the exceljs module
+        "uuid": require.resolve('uuid'),
+        "axios": require.resolve('axios')
+    },
+    "setupFilesAfterEnv": [
+        "./setupAfterEnv.js"
+    ],
+    "transformIgnorePatterns": [
+        "ace-node\\.js"
+    ]
+}
