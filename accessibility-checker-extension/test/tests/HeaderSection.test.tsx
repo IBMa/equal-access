044825a9cd147a4a8e8d618f3e6e39c22272e1eb
Tom Brunet <thbrunet@us.ibm.com>
2023-10-13 15:28:38 -0500

    Check first button

--- accessibility-checker-extension/test/tests/HeaderSection.test.tsx
+++ accessibility-checker-extension/test/tests/HeaderSection.test.tsx
@@ -12,4 +12,35 @@ describe("HeaderSection", () => {
         setupTest(<HeaderSection />)
         await (expect(document) as any).toBeAccessible();
     });
+
+    describe("First button", () => {
+        test("Label is 'Help'", async () => {
+            let button = document.body.querySelector("button");
+            let labelId = button?.getAttribute("aria-labelledby") || "";
+            let label = document.getElementById(labelId);
+            expect(label?.textContent).toEqual("Help");
+        });
+
+        test("Click opens quickGuideAC.html", async () => {
+            let button = document.body.querySelector("button");
+            // Expect click of the help button to open quickGuideAC.html
+            window.chrome = { runtime: {
+                    getURL: (url: string) => url
+            } } as any
+
+            (window as any).open = (url: string, target: string) => {
+                expect(url).toEqual("quickGuideAC.html");
+                expect(target).toEqual("_blank");
+            };
+            
+            button!.click();
+        });
+    });
+
+    test("Second button is settings", async () => {
+        let button = document.body.querySelectorAll("button")[1];
+        let labelId = button?.getAttribute("aria-labelledby") || "";
+        let label = document.getElementById(labelId);
+        expect(label?.textContent).toEqual("Settings");
+    });
 });
