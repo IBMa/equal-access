25f31a228d45d65083dfaf5b81ed4f106fb28533
Josiah Hoskins <Josiah.Hoskins@ibm.com>
2024-01-09 09:46:37 -0600

    Issues found is now tab stop and opens summary with RETURN and closes summary with RETURN Learn more now shows on RETURN (still working on close)

--- accessibility-checker-extension/src/ts/devtools/components/reportTreeGrid.tsx
+++ accessibility-checker-extension/src/ts/devtools/components/reportTreeGrid.tsx
@@ -821,21 +821,23 @@ export class ReportTreeGrid<RowType extends IRowGroup> extends React.Component<R
                                                 tabIndex={-1}
                                             />
                                         </div>
-                                        {UtilIssueReact.valueToIcon(thisIssue.value, "levelIcon")} {thisIssue.ignored && <ViewOff size={16} />}{<span style={{paddingRight:"4px"}}></span>}{thisIssue.message} <a
+                                        {UtilIssueReact.valueToIcon(thisIssue.value, "levelIcon")} {thisIssue.ignored && <ViewOff size={16} />}{<span style={{paddingRight:"4px"}}></span>}{thisIssue.message} 
+                                        <Link
                                             className="hideLg cds--link hideLg cds--link--inline cds--link--sm"
                                             role="link"
-                                            tabIndex={focused ? 0 : -1}
+                                            href="#0"
+                                            // tabIndex={focused ? 0 : -1}
                                             onClick={() => {
                                                 this.onRow(group, thisIssue);
                                                 ReportTreeGrid.devtoolsAppController.openSecondary(`#${rowId} a`);
                                             }}
-                                            onKeyDown={(evt: React.KeyboardEvent) => {
-                                                if (evt.key === "Enter" || evt.key === "Return") {
-                                                    this.onRow(group, thisIssue);
-                                                    ReportTreeGrid.devtoolsAppController.openSecondary(`#${rowId} a`);
-                                                }
-                                            }}
-                                        >Learn more</a>
+                                            // onKeyDown={(evt: React.KeyboardEvent) => {
+                                            //     if (evt.key === "Enter" || evt.key === "Return") {
+                                            //         this.onRow(group, thisIssue);
+                                            //         ReportTreeGrid.devtoolsAppController.openSecondary(`#${rowId} a`);
+                                            //     }
+                                            // }}
+                                        >Learn more</Link>
                                     </div>
                                 </Column>
                             </Grid>
