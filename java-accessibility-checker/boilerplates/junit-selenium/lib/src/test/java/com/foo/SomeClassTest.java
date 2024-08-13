1dca32742a9dff0f5fb3829278826324f74f42f1
Tom Brunet <thbrunet@us.ibm.com>
2024-08-13 14:48:26 -0500

    Add boilerplate

---
+++ java-accessibility-checker/boilerplates/junit-selenium/lib/src/test/java/com/foo/SomeClassTest.java
@@ -0,0 +1,72 @@
+/******************************************************************************
+    Copyright:: 2024- IBM, Inc
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ *****************************************************************************/
+package com.ibm.able;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.ibm.able.engine.ACReport;
+import com.ibm.able.engine.ACReport.Result;
+import com.ibm.able.report.BaselineManager.eAssertResult;
+
+public class SomeClassTest {
+    private static ChromeDriver driver;
+
+    /**
+     * Setup a Selenium Chrome environment before tests
+     */
+    @BeforeClass public static void setup() {
+        String workingDir = System.getProperty("user.dir");
+        String chromeDriverDir = System.getenv("chromedriverpath");
+        ChromeOptions options = new ChromeOptions();  
+        if (chromeDriverDir == null) {
+            chromeDriverDir = workingDir+"/src/test/resources/chromedriver-mac-arm64/chromedriver";
+        } else {
+            options.setBinary(System.getenv("chromebinpath"));
+        }
+        System.setProperty("webdriver.chrome.driver", chromeDriverDir);
+        options.addArguments("--headless=new");
+        try {
+            SomeClassTest.driver = new ChromeDriver(options);
+        } catch (SessionNotCreatedException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getAdditionalInformation());
+            throw e;
+        }
+    }
+
+    /**
+     * Close Selenium Chrome environment after tests
+     */
+    @AfterClass public static void teardown() {
+        SomeClassTest.driver.close();
+        AccessibilityChecker.close();
+    }
+
+    @Test public void getCompliance() {
+        SomeClassTest.driver.get("https://altoromutual.12mc9fdq8fib.us-south.codeengine.appdomain.cloud/");
+
+        ACReport report = AccessibilityChecker.getCompliance(driver, "getComplianceTest");
+        eAssertResult resultCode = AccessibilityChecker.assertCompliance(report);
+        assertEquals(resultCode, eAssertResult.PASS);
+    }
+}
