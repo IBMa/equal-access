a4d1b4768fcb2372536c7ec3d15dd85c642f20c5
nam-singh <nam.singh@ibm.com>
2024-08-30 20:37:16 +0530

    added icons,changed css and bg color

--- report-react/src/SavedReport.tsx
+++ report-react/src/SavedReport.tsx
@@ -20,40 +20,43 @@ import ScoreCard from './ScoreCard';
 import SummScoreCard from './SummScoreCard';
 import ReportChecklist from './report/ReportChecklist';
 import ReportRules from './report/ReportRules';
-import { ComposedModal, ModalHeader, ModalBody, Grid, Column, Theme, Tabs, TabList, TabPanel, Tab, TabPanels } from '@carbon/react';
+import Violation16 from '../src/assets/Violation16.svg'
+import NeedsReview16 from '../src/assets/NeedsReview16.svg';
+import Recommendation16 from '../src/assets/Recommendation16.svg';
+import { ComposedModal, ModalHeader, ModalBody, Grid, Column, Theme, Tabs, TabList, TabPanel, Tab, TabPanels ,Dropdown} from '@carbon/react';
 
-const Violation16 = <svg version="1.1" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16">
-    <rect style={{ fill: "none" }} width="16" height="16" />
-    <path style={{ fill: "#A2191F" }} d="M8,1C4.1,1,1,4.1,1,8s3.1,7,7,7s7-3.1,7-7S11.9,1,8,1z M10.7,11.5L4.5,5.3l0.8-0.8l6.2,6.2L10.7,11.5z" />
-    <path style={{ fill: "#FFFFFF", fillOpacity: 0 }} d="M10.7,11.5L4.5,5.3l0.8-0.8l6.2,6.2L10.7,11.5z" />
-</svg>
+// const Violation16 = <svg version="1.1" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16">
+//     <rect style={{ fill: "none" }} width="16" height="16" />
+//     <path style={{ fill: "#A2191F" }} d="M8,1C4.1,1,1,4.1,1,8s3.1,7,7,7s7-3.1,7-7S11.9,1,8,1z M10.7,11.5L4.5,5.3l0.8-0.8l6.2,6.2L10.7,11.5z" />
+//     <path style={{ fill: "#FFFFFF", fillOpacity: 0 }} d="M10.7,11.5L4.5,5.3l0.8-0.8l6.2,6.2L10.7,11.5z" />
+// </svg>
 
-const NeedsReview16 = <svg version="1.1" x="0px" y="0px"
-    width="16px" height="16px" viewBox="0 0 16 16">
-    <rect style={{ fill: "none" }} width="16" height="16" />
-    <path style={{ fill: "#F1C21B" }} d="M14.9,13.3l-6.5-12C8.3,1,8,0.9,7.8,1.1c-0.1,0-0.2,0.1-0.2,0.2l-6.5,12c-0.1,0.1-0.1,0.3,0,0.5
-	C1.2,13.9,1.3,14,1.5,14h13c0.2,0,0.3-0.1,0.4-0.2C15,13.6,15,13.4,14.9,13.3z M7.4,4h1.1v5H7.4V4z M8,11.8c-0.4,0-0.8-0.4-0.8-0.8
-	s0.4-0.8,0.8-0.8c0.4,0,0.8,0.4,0.8,0.8S8.4,11.8,8,11.8z"/>
-    <g>
-        <g>
-            <g>
-                <rect x="7.45" y="4" width="1.1" height="5" />
-            </g>
-        </g>
-        <g>
-            <g>
-                <circle cx="8" cy="11" r="0.8" />
-            </g>
-        </g>
-    </g>
-</svg>
+// const NeedsReview16 = <svg version="1.1" x="0px" y="0px"
+//     width="16px" height="16px" viewBox="0 0 16 16">
+//     <rect style={{ fill: "none" }} width="16" height="16" />
+//     <path style={{ fill: "#F1C21B" }} d="M14.9,13.3l-6.5-12C8.3,1,8,0.9,7.8,1.1c-0.1,0-0.2,0.1-0.2,0.2l-6.5,12c-0.1,0.1-0.1,0.3,0,0.5
+// 	C1.2,13.9,1.3,14,1.5,14h13c0.2,0,0.3-0.1,0.4-0.2C15,13.6,15,13.4,14.9,13.3z M7.4,4h1.1v5H7.4V4z M8,11.8c-0.4,0-0.8-0.4-0.8-0.8
+// 	s0.4-0.8,0.8-0.8c0.4,0,0.8,0.4,0.8,0.8S8.4,11.8,8,11.8z"/>
+//     <g>
+//         <g>
+//             <g>
+//                 <rect x="7.45" y="4" width="1.1" height="5" />
+//             </g>
+//         </g>
+//         <g>
+//             <g>
+//                 <circle cx="8" cy="11" r="0.8" />
+//             </g>
+//         </g>
+//     </g>
+// </svg>
 
-const Recommendation16 = <svg version="1.1" x="0px" y="0px"
-    width="16px" height="16px" viewBox="0 0 16 16">
-    <rect style={{ fill: "none" }} width="16" height="16" />
-    <path style={{ fill: "#0043CE" }} d="M14,15H2c-0.6,0-1-0.4-1-1V2c0-0.6,0.4-1,1-1h12c0.6,0,1,0.4,1,1v12C15,14.6,14.6,15,14,15z" />
-    <text transform="matrix(1 0 0 1 5.9528 12.5044)" style={{ fill: "#FFFFFF", fontFamily: "IBMPlexSerif", fontSize: "12.9996px" }}>i</text>
-</svg>
+// const Recommendation16 = <svg version="1.1" x="0px" y="0px"
+//     width="16px" height="16px" viewBox="0 0 16 16">
+//     <rect style={{ fill: "none" }} width="16" height="16" />
+//     <path style={{ fill: "#0043CE" }} d="M14,15H2c-0.6,0-1-0.4-1-1V2c0-0.6,0.4-1,1-1h12c0.6,0,1,0.4,1,1v12C15,14.6,14.6,15,14,15z" />
+//     <text transform="matrix(1 0 0 1 5.9528 12.5044)" style={{ fill: "#FFFFFF", fontFamily: "IBMPlexSerif", fontSize: "12.9996px" }}>i</text>
+// </svg>
 
 interface SavedReportProps {
     reportData: ISavedReportData | null
@@ -61,12 +64,15 @@ interface SavedReportProps {
 
 interface SavedReportState {
     selectedItem: IReportItem | null
+    reportViewState:'Requirements'|'Rules'|'Element roles'
 }
 
 export class SavedReport extends React.Component<SavedReportProps, SavedReportState> {
     state: SavedReportState = {
-        selectedItem: null
+        selectedItem: null,
+        reportViewState: "Element roles",
     }
+
 
     selectItem(item: IReportItem) {
         this.setState({ selectedItem: item });
@@ -75,8 +81,12 @@ export class SavedReport extends React.Component<SavedReportProps, SavedReportSt
     clearItem() {
         this.setState({ selectedItem: null });
     }
+  
+
 
     render() {
+        const viewItems = ["Element roles", "Requirements","Rules"];
+
         if (!this.props.reportData) {
             return <React.Fragment>Report Error</React.Fragment>
         }
@@ -146,6 +156,22 @@ export class SavedReport extends React.Component<SavedReportProps, SavedReportSt
                         <Grid>
                             <Column sm={4} md={8} lg={{offset: 4, span: 12}}>
                                 <div className="summReport">
+                                <Dropdown
+                                    className="viewMulti"
+                                    ariaLabel="Select report view"
+                                    // disabled={totalCount === 0}
+                                    id="reportView"
+                                    size="sm" 
+                                    items={viewItems}
+                                    light={false}
+                                    type="default"
+                                    style={{width:"160px", float: "right"}}
+                                    selectedItem={this.state.reportViewState}
+                                    onChange={async (evt: any) => {
+                                        // set state
+                                        this.setState({ reportViewState: evt.selectedItem });
+                                    }}
+                                />
                                     <Tabs>
                                         <TabList aria-label="Report details">
                                             <Tab>Requirements</Tab>
