ece60c69f6a60d7e610813d9d125101ac9a2d95d
nam-singh <nam.singh@ibm.com>
2024-09-05 19:19:42 +0530

    changes in utilimages

--- report-react/src/util/UtilImages.tsx
+++ report-react/src/util/UtilImages.tsx
@@ -1,3 +1,21 @@
+/******************************************************************************
+  Copyright:: 2020- IBM, Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*****************************************************************************/
+
+
+//svg images for report
 export const Violation16 = <svg version="1.1" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16">
     <rect style={{ fill: "none" }} width="16" height="16" />
     <path style={{ fill: "#da1e28" }} d="M8,1C4.1,1,1,4.1,1,8s3.1,7,7,7s7-3.1,7-7S11.9,1,8,1z M10.7,11.5L4.5,5.3l0.8-0.8l6.2,6.2L10.7,11.5z" />
