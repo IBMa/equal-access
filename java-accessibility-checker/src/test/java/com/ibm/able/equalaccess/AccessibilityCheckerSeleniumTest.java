613eedc37bad749a6585f05f54e8e8331736ef6c
Shunguo <shunguoy@us.ibm.com>
2024-10-29 14:50:21 -0500

    skip three test files in java test #1974

--- java-accessibility-checker/src/test/java/com/ibm/able/equalaccess/AccessibilityCheckerSeleniumTest.java
+++ java-accessibility-checker/src/test/java/com/ibm/able/equalaccess/AccessibilityCheckerSeleniumTest.java
@@ -242,6 +242,10 @@ public class AccessibilityCheckerSeleniumTest {
 
                 // Misc
                 // path.join(testRootDir, "aria_banner_label_unique_ruleunit", "validLandMarks-testCaseFromAnn.html"),
+
+                Paths.get(testRootDir.getAbsolutePath(), "target_spacing_sufficient_ruleunit","link_text.html"),
+                Paths.get(testRootDir.getAbsolutePath(), "target_spacing_sufficient_ruleunit","element_inline2.html"),
+                Paths.get(testRootDir.getAbsolutePath(), "target_spacing_sufficient_ruleunit","link_inline_with_block.html")
             }));
 
             for (File testFile: testFiles) {
