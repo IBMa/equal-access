e680383ca64764cae4c9309a594421ed3bc6bfe3
Tom Brunet <thbrunet@us.ibm.com>
2025-01-08 12:57:42 -0600

    Try no sandbox

--- accessibility-checker/test-act-w3/main.mjs
+++ accessibility-checker/test-act-w3/main.mjs
@@ -12,7 +12,7 @@ import * as fs from "fs";
     }
     
     // Setup the Puppeteer test environment
-    let browser = await puppeteer.launch({ headless: 'shell', ignoreHTTPSErrors: true });
+    let browser = await puppeteer.launch({ headless: 'shell', args: ['--no-sandbox'], ignoreHTTPSErrors: true });
     let pupPage = await browser.newPage();
     await pupPage.setRequestInterception(true);
     pupPage.on('request', request => {
